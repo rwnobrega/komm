f0b5580e8862eda9db9c18d37ba8f69176711f82
Roberto Nobrega <rwnobrega@gmail.com>
2018-05-01 13:24:10 -0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(0, os.path.abspath('../komm/'))
 
 # -- General configuration ------------------------------------------------
 
