a33e99c8ba34ccf0c9f4c2ccb44ff5f31c1f8f62
Roberto Nobrega <rwnobrega@gmail.com>
2018-05-02 22:50:13 -0300

    Update channels.py

--- komm/channels.py
+++ komm/channels.py
@@ -10,15 +10,13 @@ __all__ = ['AWGNChannel',
 
 class AWGNChannel:
     """
-    Additive white gaussian noise (AWGN) channel.
-
-    The *additive white gaussian noise* (*AWGN*) channel :cite:`Cover.Thomas.06` (Ch. 9) is defined by
+    Additive white gaussian noise (AWGN) channel. It is defined by
 
     .. math::
 
         Y_n = X_n + Z_n,
 
-    where :math:`X_n` is the channel *input signal*, :math:`Y_n` is the channel *output signal*, and :math:`Z_n` is the *noise*, which is :term:`i.i.d.` according to a gaussian distribution with zero mean. The channel *signal-to-noise ratio* (*SNR*) is calculated by
+    where :math:`X_n` is the channel *input signal*, :math:`Y_n` is the channel *output signal*, and :math:`Z_n` is the *noise*, which is :term:`i.i.d.` according to a gaussian distribution with zero mean. The channel *signal-to-noise ratio* is calculated by
 
     .. math::
 
@@ -26,16 +24,9 @@ class AWGNChannel:
 
     where :math:`P = \\mathrm{E}[X^2_n]` is the average power of the input signal, and :math:`N = \\mathrm{E}[Z^2_n]` is the average power (and variance) of the noise.
 
-    The constructor takes the channel SNR and the input signal power as parameters.
+    References: :cite:`Cover.Thomas.06` (Ch. 9)
 
-    To invoke the channel for a given input, just
-
-    Parameters
-    ==========
-    snr : `float`, optional
-        The channel signal-to-noise ratio :math:`\\mathrm{SNR}` (linear, not decibel). Default value is :code:`np.inf`.
-    signal_power : `float` or `str`, optional
-        The input signal power :math:`P`.  If set to the string :code:`'measured'`, every time the channel is invoked the input signal power will be computed from the input (i.e., its squared Euclidean norm). Default value is :code:`1.0`.
+    To invoke the channel, call the object giving the input signal as parameter (see example below).
 
     Examples
     ========
@@ -47,13 +38,22 @@ class AWGNChannel:
             0.75899613,  2.86905731,  1.16039474, -1.02437047,  2.91492338])
     """
     def __init__(self, snr=np.inf, signal_power=1.0):
+        """Constructor for the class. It expects the following parameters:
+
+        snr : :obj:`float`, optional
+            The channel signal-to-noise ratio :math:`\\mathrm{SNR}` (linear, not decibel). The default value is :code:`np.inf`.
+
+        signal_power : :obj:`float` or :obj:`str`, optional
+            The input signal power :math:`P`.  If equal to the string :code:`'measured'`, then every time the channel is invoked the input signal power will be computed from the input itself (i.e., its squared Euclidean norm). The default value is :code:`1.0`.
+        """
+
         self._snr = snr
         self._signal_power = signal_power
 
     @property
     def snr(self):
         """
-        The channel signal-to-noise ratio :math:`\\mathrm{SNR}` (linear, not decibel).
+        The channel signal-to-noise ratio :math:`\\mathrm{SNR}` (linear, not decibel). This is a read-and-write property.
         """
         return self._snr
 
@@ -64,7 +64,7 @@ class AWGNChannel:
     @property
     def signal_power(self):
         """
-        The input signal power :math:`P`.
+        The input signal power :math:`P`. This is a read-and-write property.
         """
         return self._signal_power
 
@@ -109,16 +109,11 @@ class AWGNChannel:
 
 class DiscreteMemorylessChannel:
     """
-    Discrete memoryless channel (DMC).
+    Discrete memoryless channel (DMC). It is defined by an *input alphabet* :math:`\\mathcal{X}`, an *output alphabet* :math:`\\mathcal{Y}`, and a *transition matrix* :math:`p_{Y \mid X}`. Here, for simplicity, the input and output alphabets are always taken as :math:`\\mathcal{X} = \\{ 0, 1, \ldots, |\\mathcal{X}| - 1 \\}` and :math:`\\mathcal{Y} = \\{ 0, 1, \ldots, |\\mathcal{Y}| - 1 \\}`, respectively. The transition matrix :math:`p_{Y \mid X}`, of size :math:`|\\mathcal{X}|`-by-:math:`|\\mathcal{Y}|`, gives the conditional probability of receiving :math:`Y = y` given that :math:`X = x` is transmitted.
 
-    A *discrete memoryless channel* (*DMC*) :cite:`Cover.Thomas.06` (Ch. 7) is defined by an *input alphabet* :math:`\\mathcal{X}`, an *output alphabet* :math:`\\mathcal{Y}`, and a *transition matrix* :math:`p_{Y \mid X}`.
+    References: :cite:`Cover.Thomas.06` (Ch. 7)
 
-    The constructor expects the transition matrix, of size :math:`|\\mathcal{X}|`-by-:math:`|\\mathcal{Y}|`, as its sole parameter. The input and output alphabets are always taken as :math:`\\mathcal{X} = \\{ 0, 1, \ldots, |\\mathcal{X}| - 1 \\}` and :math:`\\mathcal{Y} = \\{ 0, 1, \ldots, |\\mathcal{Y}| - 1 \\}`, respectively.
-
-    Parameters
-    ==========
-    snr : `float`
-        The channel signal-to-noise ratio :math:`\\mathrm{SNR}`. Default value is :code:`1.0`.
+    To invoke the channel, call the object giving the input signal as parameter (see example below).
 
     Examples
     ========
@@ -129,6 +124,12 @@ class DiscreteMemorylessChannel:
     array([0, 2, 0, 2, 1, 1, 0, 0, 0, 2])
     """
     def __init__(self, transition_matrix):
+        """
+        Constructor for the class. It expects the following parameter:
+
+        transition_matrix : :obj:`numpy.ndarray` of :obj:`float`
+            The channel transition matrix :math:`p_{Y \mid X}`.
+        """
         self._transition_matrix = np.array(transition_matrix, dtype=np.float)
         self._input_cardinality, self._output_cardinality = self._transition_matrix.shape
         self._arimoto_blahut_kwargs = {'max_iters': 1000, 'error_tolerance': 1e-12}
@@ -136,27 +137,27 @@ class DiscreteMemorylessChannel:
     @property
     def transition_matrix(self):
         """
-        The channel transition matrix :math:`p_{Y \mid X}`.
+        The channel transition matrix :math:`p_{Y \mid X}`. This property is read-only.
         """
         return self._transition_matrix
 
     @property
     def input_cardinality(self):
         """
-        The channel input cardinality :math:`|\\mathcal{X}|`.
+        The channel input cardinality :math:`|\\mathcal{X}|`. This property is read-only.
         """
         return self._input_cardinality
 
     @property
     def output_cardinality(self):
         """
-        The channel output cardinality :math:`|\\mathcal{Y}|`.
+        The channel output cardinality :math:`|\\mathcal{Y}|`. This property is read-only.
         """
         return self._output_cardinality
 
     def capacity(self):
         """
-        Returns the channel capacity, calculated by the Arimoto--Blahut algorithm :cite:`Cover.Thomas.06` (Sec. 10.8).
+        Returns the channel capacity, calculated by the Arimoto--Blahut algorithm. See :cite:`Cover.Thomas.06` (Sec. 10.8).
 
         Examples
         ========
@@ -165,40 +166,55 @@ class DiscreteMemorylessChannel:
         0.1616318609548566
         """
         initial_guess = np.ones(self._input_cardinality, dtype=np.float) / self._input_cardinality
-        optimal_input_pmf = arimoto_blahut(self._transition_matrix, initial_guess, **self._arimoto_blahut_kwargs)
+        optimal_input_pmf = self._arimoto_blahut(self._transition_matrix, initial_guess, **self._arimoto_blahut_kwargs)
         return mutual_information(optimal_input_pmf, self._transition_matrix)
 
     def __call__(self, input_sequence):
-        return discrete_memoryless_channel(input_sequence, self._output_cardinality, self._transition_matrix)
+        output_sequence = [np.random.choice(self.output_cardinality, p=self.transition_matrix[input_symbol])
+                           for input_symbol in input_sequence]
+        return np.array(output_sequence)
 
     def __repr__(self):
         args = 'transition_matrix={}'.format(self._transition_matrix.tolist())
         return '{}({})'.format(self.__class__.__name__, args)
 
+    @staticmethod
+    def _arimoto_blahut(transition_matrix, initial_guess, max_iters, error_tolerance):
+        """
+        Arimoto--Blahut algorithm for channel capacity. See :cite:`Cover.Thomas.06` (Sec. 10.8).
+        """
+        p = transition_matrix
+        r = initial_guess
+        last_r = np.full_like(r, fill_value=np.inf)
+        iters = 0
+        while iters < max_iters and np.max(np.abs(r - last_r)) > error_tolerance:
+            last_r = r
+            q = r[np.newaxis].T * p
+            q /= np.sum(q, axis=0)
+            r = np.product(q ** p, axis=1)
+            r /= np.sum(r, axis=0)
+            iters += 1
+        return r
+
 
 class BinarySymmetricChannel(DiscreteMemorylessChannel):
     """
-    Binary symmetric channel (BSC).
-
-    The *binary symmetric channel* (*BSC*) :cite:`Cover.Thomas.06` (Sec. 7.1.4) is a :class:`discrete memoryless channel <DiscreteMemorylessChannel>` with input and output alphabets given by :math:`\\mathcal{X} = \\mathcal{Y} = \\{ 0, 1 \\}`, and transition matrix given by
+    Binary symmetric channel (BSC). It is :class:`discrete memoryless channel <DiscreteMemorylessChannel>` with input and output alphabets given by :math:`\\mathcal{X} = \\mathcal{Y} = \\{ 0, 1 \\}`, and transition matrix given by
 
     .. math::
 
         p_{Y \mid X} = \\begin{bmatrix} 1-p & p \\\\ p & 1-p \\end{bmatrix},
 
-    where the parameter :math:`p` is called the *crossover probability* of the channel.
-
-    Equivalently, a BSC with crossover probability :math:`p` may be defined by
+    where the parameter :math:`p` is called the *crossover probability* of the channel. Equivalently, a BSC with crossover probability :math:`p` may be defined by
 
     .. math::
         Y_n = X_n + Z_n,
 
     where :math:`Z_n \sim \mathrm{Bernoulli}(p)` are :term:`i.i.d.` random variables.
 
-    Parameters
-    ==========
-    crossover_probability : `float`, optional
-        The channel crossover probability :math:`p`. Must satisfy :math:`0 \leq p \leq 1`. Default value is :code:`0.0`.
+    References: :cite:`Cover.Thomas.06` (Sec. 7.1.4)
+
+    To invoke the channel, call the object giving the input signal as parameter (see example below).
 
     Examples
     ========
@@ -209,20 +225,25 @@ class BinarySymmetricChannel(DiscreteMemorylessChannel):
     array([0, 1, 1, 1, 0, 0, 0, 1, 0, 0])
     """
     def __init__(self, crossover_probability=0.0):
+        """
+        Constructor for the class. It expects the following parameter:
+
+        crossover_probability : :obj:`float`, optional
+            The channel crossover probability :math:`p`. Must satisfy :math:`0 \leq p \leq 1`. The default value is :code:`0.0`.
+        """
         super().__init__([[1 - crossover_probability, crossover_probability], [crossover_probability, 1 - crossover_probability]])
         self._crossover_probability = crossover_probability
 
     @property
     def crossover_probability(self):
         """
-        The channel crossover probability :math:`p`.
+        The channel crossover probability :math:`p`. This property is read-only.
         """
         return self.crossover_probability
 
     def capacity(self):
         """
-        Returns the channel capacity, given by :math:`C = 1 - \\mathcal{H}(p)`
-        :cite:`Cover.Thomas.06` (Sec. 7.1.4).
+        Returns the channel capacity, given by :math:`C = 1 - \\mathcal{H}(p)`. See :cite:`Cover.Thomas.06` (Sec. 7.1.4).
 
         Examples
         ========
@@ -233,7 +254,7 @@ class BinarySymmetricChannel(DiscreteMemorylessChannel):
         return 1.0 - entropy(np.array([self._crossover_probability, 1.0 - self._crossover_probability]))
 
     def __call__(self, input_sequence):
-        error_pattern = bernoulli_iid(self._crossover_probability, input_sequence.size)
+        error_pattern = (np.random.rand(input_sequence.size) < self._crossover_probability).astype(np.int)
         return (input_sequence + error_pattern) % 2
 
     def __repr__(self):
@@ -243,9 +264,7 @@ class BinarySymmetricChannel(DiscreteMemorylessChannel):
 
 class BinaryErasureChannel(DiscreteMemorylessChannel):
     """
-    Binary erasure channel (BSC).
-
-    The *binary erasure channel* (*BEC*) :cite:`Cover.Thomas.06` (Sec. 7.1.5) is a :class:`discrete memoryless channel <DiscreteMemorylessChannel>` with input alphabet :math:`\\mathcal{X} = \\{ 0, 1 \\}`, output alphabet :math:`\\mathcal{Y} = \\{ 0, 1, 2 \\}`, and transition matrix given by
+    Binary erasure channel (BSC). It a :class:`discrete memoryless channel <DiscreteMemorylessChannel>` with input alphabet :math:`\\mathcal{X} = \\{ 0, 1 \\}`, output alphabet :math:`\\mathcal{Y} = \\{ 0, 1, 2 \\}`, and transition matrix given by
 
     .. math::
 
@@ -257,10 +276,9 @@ class BinaryErasureChannel(DiscreteMemorylessChannel):
 
     where the parameter :math:`\\epsilon` is called the *erasure probability* of the channel.
 
-    Parameters
-    ==========
-    erasure_probability : `float`, optional
-        The channel erasure probability :math:`\\epsilon`. Must satisfy :math:`0 \leq \\epsilon \leq 1`. Default value is :code:`0.0`.
+    References: :cite:`Cover.Thomas.06` (Sec. 7.1.5)
+
+    To invoke the channel, call the object giving the input signal as parameter (see example below).
 
     Examples
     ========
@@ -271,19 +289,25 @@ class BinaryErasureChannel(DiscreteMemorylessChannel):
     array([1, 1, 1, 2, 0, 0, 1, 0, 1, 0])
     """
     def __init__(self, erasure_probability=0.0):
+        """
+        Constructor for the class. It expects the following parameter:
+
+        erasure_probability : :obj:`float`, optional
+            The channel erasure probability :math:`\\epsilon`. Must satisfy :math:`0 \leq \\epsilon \leq 1`. Default value is :code:`0.0`.
+        """
         super().__init__([[1 - erasure_probability, 0, erasure_probability], [0, 1 - erasure_probability, erasure_probability]])
         self._erasure_probability = erasure_probability
 
     @property
     def erasure_probability(self):
         """
-        The channel erasure probability :math:`\\epsilon`.
+        The channel erasure probability :math:`\\epsilon`. This property is read-only.
         """
         return self._erasure_probability
 
     def capacity(self):
         """
-        Returns the channel capacity, given by :math:`C = 1 - \\epsilon`.
+        Returns the channel capacity, given by :math:`C = 1 - \\epsilon`.  See :cite:`Cover.Thomas.06` (Sec. 7.1.5).
 
         Examples
         ========
@@ -294,7 +318,7 @@ class BinaryErasureChannel(DiscreteMemorylessChannel):
         return 1.0 - self._erasure_probability
 
     def __call__(self, input_sequence):
-        erasure_pattern = bernoulli_iid(self._erasure_probability, input_sequence.size) == 1
+        erasure_pattern = (np.random.rand(input_sequence.size) < self._erasure_probability)
         output_sequence = np.copy(input_sequence)
         output_sequence[erasure_pattern] = 2
         return output_sequence
@@ -302,37 +326,3 @@ class BinaryErasureChannel(DiscreteMemorylessChannel):
     def __repr__(self):
         args = 'erasure_probability={}'.format(self._erasure_probability)
         return '{}({})'.format(self.__class__.__name__, args)
-
-
-
-
-
-
-
-def bernoulli_iid(success_probability, size):
-    """Bernoulli iid process."""
-    return (np.random.rand(size) < success_probability).astype(np.int)
-
-
-def discrete_memoryless_channel(input_sequence, output_cardinality, transition_matrix):
-    output_sequence = [np.random.choice(output_cardinality, p=transition_matrix[input_symbol])
-                       for input_symbol in input_sequence]
-    return np.array(output_sequence)
-
-
-def arimoto_blahut(transition_matrix, initial_guess, max_iters, error_tolerance):
-    """
-    Arimoto--Blahut algorithm for channel capacity :cite:`Cover.Thomas.06` (p. 335).
-    """
-    p = transition_matrix
-    r = initial_guess
-    last_r = np.full_like(r, fill_value=np.inf)
-    iters = 0
-    while iters < max_iters and np.max(np.abs(r - last_r)) > error_tolerance:
-        last_r = r
-        q = r[np.newaxis].T * p
-        q /= np.sum(q, axis=0)
-        r = np.product(q ** p, axis=1)
-        r /= np.sum(r, axis=0)
-        iters += 1
-    return r
