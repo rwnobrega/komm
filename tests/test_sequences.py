dde0f95149e3c8d18436fe9aff37d839a843d591
Roberto Nobrega <rwnobrega@gmail.com>
2018-05-09 19:25:01 -0300

    Update test_sequences.py

--- tests/test_sequences.py
+++ tests/test_sequences.py
@@ -14,13 +14,11 @@ def test_lfsr_sequence():
     assert b2h(lfsr.bit_sequence[:200]) == '0029c2be33e317f3c10ff3e8cd4ddb2e5a28ef55db079896dc'
 
 
-@pytest.mark.parametrize('num_states', range(2, 13))
+@pytest.mark.parametrize('num_states', range(2, 17))
 def test_lfsr_mls(num_states):
     lfsr = komm.LFSRSequence.maximum_length_sequence(num_states)
     assert num_states == lfsr.feedback_polynomial.degree
     assert lfsr.length == 2**lfsr.feedback_polynomial.degree - 1
-
     cyclic_autocorrelation_expected = np.full(lfsr.length, fill_value=-1, dtype=np.int)
     cyclic_autocorrelation_expected[0] = lfsr.length
-
-    assert(np.allclose(lfsr.cyclic_autocorrelation(), cyclic_autocorrelation_expected))
+    assert(np.array_equal(lfsr.cyclic_autocorrelation(), cyclic_autocorrelation_expected))
