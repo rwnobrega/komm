ebe2a7304f0e2e55a15e80c3293360198b53a943
Roberto Nobrega <rwnobrega@gmail.com>
2018-05-14 22:39:36 -0300

    Update source coding test

---
+++ tests/test_source_coding.py
@@ -0,0 +1,25 @@
+import pytest
+
+import numpy as np
+import komm
+
+
+def test_prefix_code():
+    mapping_1 = [(0,0,0), (0,0,1), (0,1,0), (0,1,1), (1,0,0)]
+    mapping_2 = [(0,0), (1,0), (1,1), (0,1,0), (0,1,1)]
+
+    prefix_code_1 = komm.PrefixCode(mapping_1)
+    prefix_code_2 = komm.PrefixCode(mapping_2)
+    x = [3, 0, 1, 1, 1, 0, 2, 0]
+    y_1 = [0,1,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0]
+    y_2 = [0,1,0,0,0,1,0,1,0,1,0,0,0,1,1,0,0]
+    pmf = [0.4, 0.2, 0.2, 0.1, 0.1]
+
+    assert np.array_equal(prefix_code_1.encode(x), y_1)
+    assert np.array_equal(prefix_code_1.decode(y_1), x)
+    assert np.array_equal(prefix_code_2.encode(x), y_2)
+    assert np.array_equal(prefix_code_2.decode(y_2), x)
+
+    assert prefix_code_1.average_length(pmf) == 3.0
+    assert prefix_code_2.average_length(pmf) == 2.2
+
