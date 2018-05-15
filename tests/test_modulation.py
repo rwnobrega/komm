2507239e81a685dcffa94a66c277e86140c708f6
Roberto Nobrega <rwnobrega@gmail.com>
2018-05-15 15:42:35 -0300

    Add a test for QAM

--- tests/test_modulation.py
+++ tests/test_modulation.py
@@ -30,8 +30,11 @@ def test_psk_modulation():
 
 def test_qam_modulation():
     qam4 = komm.QAModulation(4)
+    qam8 = komm.QAModulation((4,2))
     qam16 = komm.QAModulation(16)
-    assert np.allclose(qam4.constellation, [-1.0 -1.0j, 1.0 -1.0j, -1.0 + 1.0j, 1.0 + 1.0j])
+    assert np.allclose(qam4.constellation, [-1.0 - 1.0j, 1.0 - 1.0j, -1.0 + 1.0j, 1.0 + 1.0j])
+    assert np.allclose(qam8.constellation, [-3.0 - 1.0j, -1.0 - 1.0j, 1.0 - 1.0j, 3.0 - 1.0j,
+                                            -3.0 + 1.0j, -1.0 + 1.0j, 1.0 + 1.0j, 3.0 + 1.0j])
     assert np.allclose(qam16.constellation, [-3.0 - 3.0j, -1.0 - 3.0j, 1.0 - 3.0j, 3.0 - 3.0j,
                                              -3.0 - 1.0j, -1.0 - 1.0j, 1.0 - 1.0j, 3.0 - 1.0j,
                                              -3.0 + 1.0j, -1.0 + 1.0j, 1.0 + 1.0j, 3.0 + 1.0j,
