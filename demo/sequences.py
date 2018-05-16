92d312ccd5055cafd7448e16dd3b137f6003488d
Roberto Nobrega <rwnobrega@gmail.com>
2018-05-16 09:17:46 -0300

    Update demo on sequences

--- demo/sequences.py
+++ demo/sequences.py
@@ -5,36 +5,31 @@ import komm
 
 
 def barker_demo():
-    L = 13
-    bark = komm.BarkerSequence(length=L)
-    seq = (-1)**bark.sequence
+    bark = komm.BarkerSequence(length=13)
     plt.close('all')
     plt.figure()
-    plt.stem(np.arange(L), seq)
-    plt.title(repr(bark) + ' - ' + str(bark.sequence.tolist()))
+    plt.stem(np.arange(bark.length), bark.polar_sequence)
+    plt.title(repr(bark))
     plt.show()
 
 
 def walsh_hadamard_demo():
     had = komm.WalshHadamardSequence(length=32, index=29)
-    print(had)
-    print(had.sequence)
+    plt.close('all')
+    plt.figure()
+    plt.stem(np.arange(had.length), had.polar_sequence)
+    plt.title(repr(had))
+    plt.show()
 
 
 def lfsr_demo():
-    m = 5
-    L = 2**m - 1
     lfsr = komm.LFSRSequence(0b100101)
-    seq = (-1)**lfsr.sequence
-
-    shifts = np.arange(-2*L + 1, 2*L)
-    cyclic_acorr = np.empty_like(shifts, dtype=np.float)
-    for (i, ell) in enumerate(shifts):
-        cyclic_acorr[i] = np.dot(seq, np.roll(seq, ell)) / L
+    shifts = np.arange(-2*lfsr.length + 1, 2*lfsr.length)
+    cyclic_acorr = lfsr.cyclic_autocorrelation(shifts)
 
     plt.close('all')
     plt.figure()
-    plt.stem(np.arange(L), seq)
+    plt.stem(range(lfsr.length), lfsr.polar_sequence)
     plt.title(repr(lfsr))
     plt.figure()
     plt.plot(shifts, cyclic_acorr)
