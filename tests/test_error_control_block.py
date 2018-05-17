86e4f6c30855214a2fc7e25d3ee70b9cecdfc635
Roberto Nobrega <rwnobrega@gmail.com>
2018-05-16 22:43:57 -0300

    Add tests for GolayCode

--- tests/test_error_control_block.py
+++ tests/test_error_control_block.py
@@ -31,10 +31,33 @@ class TestHammingCode:
         codewords = self.code.codeword_table()
         assert codewords.shape == (2**k, n)
 
-    def test_exhaustive_search_hard_decoder(self):
+    def test_decoding(self):
         assert np.array_equal(self.code.decode([1, 1, 1, 1, 1, 1, 1]), [1, 1, 1, 1])
         assert np.array_equal(self.code.decode([1, 1, 1, 1, 1, 1, 0]), [1, 1, 1, 1])
         assert np.array_equal(self.code.decode([1, 0, 1, 1, 1, 1, 0]), [1, 0, 1, 1])
+
+
+class TestGolayCode:
+    code = komm.GolayCode()
+
+    def test_parameters(self):
+        n, k, d = self.code.length, self.code.dimension, self.code.minimum_distance
+        assert (n, k, d) == (23, 12, 7)
+
+    def test_weight_distributions(self):
+        assert np.array_equal(self.code.codeword_weight_distribution(), [1, 0, 0, 0, 0, 0, 0, 253, 506, 0, 0, 1288, 1288, 0, 0, 506, 253, 0, 0, 0, 0, 0, 0, 1])
+        assert np.array_equal(self.code.coset_leader_weight_distribution(), [1, 23, 253, 1771, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    def test_GH_orthogonality(self):
+        n, k = self.code.length, self.code.dimension
+        G = self.code.generator_matrix
+        H = self.code.parity_check_matrix
+        assert np.array_equal((G @ H.T) % 2, np.zeros((k, n - k), dtype=np.int))
+
+    def test_codewords(self):
+        n, k = self.code.length, self.code.dimension
+        codewords = self.code.codeword_table()
+        assert codewords.shape == (2**k, n)
 
 
 @pytest.mark.parametrize('length, generator_polynomial, parity_check_polynomial', [
