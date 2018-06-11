1f4ed6a66299f8d0c889abc0c1dee49e0237c0e1
Roberto Nobrega <rwnobrega@gmail.com>
2018-06-11 13:04:35 -0300

    Add tests for space-state representation of convolutional codes

--- tests/test_error_control_convolutional.py
+++ tests/test_error_control_convolutional.py
@@ -36,6 +36,46 @@ def test_convolutional_code():
     assert np.array_equal(code.overall_constraint_length, 2)
 
 
+def test_convolutional_space_state_representation():
+    code = komm.ConvolutionalCode(feedforward_polynomials=[[0o7, 0o5]])
+    A = code.state_matrix
+    B = code.control_matrix
+    C = code.observation_matrix
+    D = code.transition_matrix
+    assert np.array_equal(A, [[0, 1], [0, 0]])
+    assert np.array_equal(B, [[1, 0]])
+    assert np.array_equal(C, [[1, 0], [1, 1]])
+    assert np.array_equal(D, [[1, 1]])
+
+
+@pytest.mark.parametrize('feedforward_polynomials, feedback_polynomials', [
+    ([[0o7, 0o5]], None),
+    ([[0o117, 0o155]], None),
+    ([[0o31, 0o27, 0o00], [0o00, 0o12, 0o15]], None),
+    ([[0o27, 0o31]], [0o27]),
+])
+def test_convolutional_space_state_representation_2(feedforward_polynomials, feedback_polynomials):
+    code = komm.ConvolutionalCode(feedforward_polynomials, feedback_polynomials)
+    n, k, nu = code.num_output_bits, code.num_input_bits, code.overall_constraint_length
+
+    A = code.state_matrix
+    B = code.control_matrix
+    C = code.observation_matrix
+    D = code.transition_matrix
+
+    input_bits = np.random.randint(2, size=100*k)
+    output_bits = np.empty(n * input_bits.size // k, dtype=np.int)
+
+    s = np.zeros(nu, dtype=np.int)
+
+    for t, u in enumerate(np.reshape(input_bits, newshape=(-1, k))):
+        s, v = (np.dot(s, A) + np.dot(u, B)) % 2, (np.dot(s, C) + np.dot(u, D)) % 2
+        output_bits[t*n : (t+1)*n] = v
+
+    convolutional_encoder = komm.ConvolutionalStreamEncoder(code)
+    assert np.array_equal(output_bits, convolutional_encoder(input_bits))
+
+
 def test_convolutional_stream_encoder():
     # Abrantes.10, p.307
     code = komm.ConvolutionalCode(feedforward_polynomials=[[0b111, 0b101]])
