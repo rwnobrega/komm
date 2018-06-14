10d37ebb8a6d6b9441bfaf2d04faac89702a05c4
Roberto Nobrega <rwnobrega@gmail.com>
2018-06-14 17:45:06 -0300

    Add demo on pulse formatting

---
+++ demo/pulse_formatting.py
@@ -0,0 +1,82 @@
+
+# coding: utf-8
+
+# # Komm demo: Pulse formatting
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import numpy as np
+import matplotlib.pylab as plt
+import ipywidgets
+import komm
+
+
+# ## Raised cosine pulse
+
+# In[2]:
+
+
+rolloff_widget = ipywidgets.FloatSlider(min=0, max=1.0, step=0.1, value=0.5)
+
+def raised_cosine_demo(rolloff):
+    pulse = komm.RaisedCosinePulse(rolloff, length_in_symbols=20)
+    h = pulse.impulse_response
+    H = pulse.frequency_response
+    t = np.linspace(-8.0, 8.0, 1000)
+    f = np.linspace(-1.5, 1.5, 200)
+
+    _, (ax0, ax1) = plt.subplots(1, 2, figsize=(16, 5))
+    ax0.plot(t, h(t), 'b')
+    ax0.axis([-7.1, 7.1, -.3, 1.1])
+    ax0.set_title('Raised cosine pulse (waveform)')
+    ax0.set_xlabel('$t$')
+    ax0.set_ylabel('$h(t)$')
+    ax0.grid()
+    ax1.plot(f, H(f), 'r')
+    ax1.axis([-1.1, 1.1, -.1, 1.1])
+    ax1.set_title('Raised cosine pulse (spectrum)')
+    ax1.set_xlabel('$f$')
+    ax1.set_ylabel('$H(f)$')
+    ax1.grid()
+    plt.show()
+    
+ipywidgets.interact(raised_cosine_demo, rolloff=rolloff_widget);
+
+
+# ## Gaussian pulse
+
+# In[3]:
+
+
+half_power_bandwidth_widget = ipywidgets.FloatSlider(min=0.05, max=1.0, step=0.01, value=0.5)
+
+def gaussian_pulse_demo(half_power_bandwidth):
+    pulse = komm.GaussianPulse(half_power_bandwidth, length_in_symbols=4)
+    h = pulse.impulse_response
+    H = pulse.frequency_response
+    t = np.linspace(-8.0, 8.0, 1000)
+    f = np.linspace(-4.0, 4.0, 500)
+
+    _, (ax0, ax1) = plt.subplots(1, 2, figsize=(16, 5))
+    ax0.plot(t, h(t), 'b')
+    ax0.axis([-7.1, 7.1, -.1, 1.1])
+    ax0.set_title('Gaussian pulse (waveform)')
+    ax0.set_xlabel('$t$')
+    ax0.set_ylabel('$h(t)$')
+    ax0.grid()
+    ax1.plot(f, H(f), 'r')
+    ax1.plot([-4.0, 4.0], [H(0.0) / np.sqrt(2), H(0.0) / np.sqrt(2)], linestyle='dashed', color='gray')
+    ax1.plot([half_power_bandwidth, half_power_bandwidth], [-0.1*H(0.0), 1.1 * H(0.0)], linestyle='dashed', color='gray')
+    ax1.plot([-half_power_bandwidth, -half_power_bandwidth], [-0.1*H(0.0), 1.1 * H(0.0)], linestyle='dashed', color='gray')
+    ax1.axis([-2.0, 2.0, -0.1*H(0.0), 1.1 * H(0.0)])
+    ax1.set_title('Gaussian pulse (spectrum)')
+    ax1.set_xlabel('$f$')
+    ax1.set_ylabel('$H(f)$')
+    ax1.grid()
+    plt.show()
+    
+ipywidgets.interact(gaussian_pulse_demo, half_power_bandwidth=half_power_bandwidth_widget);
+
