1bac2e8a3920a25055deef6acab04b3318d7a2c1
Roberto Nobrega <rwnobrega@gmail.com>
2018-06-14 14:40:48 -0300

    Update binary sequences demo

--- demo/binary_sequences.py
+++ demo/binary_sequences.py
@@ -8,11 +8,9 @@
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-from ipywidgets import interact, interactive, fixed, interact_manual
-import ipywidgets as widgets
-
 import numpy as np
 import matplotlib.pylab as plt
+import ipywidgets
 import komm
 
 
@@ -26,7 +24,7 @@ def barker_demo(index):
     barker = komm.BarkerSequence(length=length)
     shifts = np.arange(-2*length + 1, 2*length)
     _, (ax0, ax1) = plt.subplots(1, 2, figsize=(12, 4))
-    ax0.stem(np.arange(barker.length), barker.polar_sequence)
+    ax0.stem(np.arange(length), barker.polar_sequence)
     ax0.set_title(repr(barker))
     ax0.set_xlabel('$n$')
     ax0.set_ylabel('$a[n]$')
@@ -36,11 +34,11 @@ def barker_demo(index):
     ax1.set_title('Autocorrelation')
     ax1.set_xlabel('$\\ell$')
     ax1.set_ylabel('$R[\\ell]$')
-    ax1.set_xticks([-length + 1, 0, length - 1])
+    ax1.set_xticks([-length, 0, length])
     ax1.set_yticks(np.arange(-1, length + 1))
     plt.show()
 
-interact(barker_demo, index=(0, 6));
+ipywidgets.interact(barker_demo, index=(0, 6));
 
 
 # ## Walsh-Hadamard sequence
@@ -48,31 +46,50 @@ interact(barker_demo, index=(0, 6));
 # In[3]:
 
 
-walsh_hadamard = komm.WalshHadamardSequence(length=32, index=29)
-fig = plt.figure()
-ax = plt.axes()
-ax.stem(np.arange(walsh_hadamard.length), walsh_hadamard.polar_sequence)
-ax.set_title(repr(walsh_hadamard))
-plt.show()
+log_length_widget = ipywidgets.IntSlider(min=0, max=7, step=1, value=3)
+index_widget = ipywidgets.IntSlider(min=0, max=7, step=1, value=0)
+
+def update_index_widget(*args):
+    index_widget.max = 2 ** log_length_widget.value - 1
+log_length_widget.observe(update_index_widget, 'value')
+
+def walsh_hadamard_demo(log_length, ordering, index):
+    length = 2**log_length
+    walsh_hadamard = komm.WalshHadamardSequence(length=length, ordering=ordering, index=index)
+    ax = plt.axes()
+    ax.stem(np.arange(length), walsh_hadamard.polar_sequence)
+    ax.set_title(repr(walsh_hadamard))
+    ax.set_xlabel('$n$')
+    ax.set_ylabel('$a[n]$')
+    ax.set_yticks([-1, 0, 1])
+    ax.set_ylim([-1.2, 1.2])
+    plt.show()
+    
+ipywidgets.interact(walsh_hadamard_demo, log_length=log_length_widget, ordering=['natural', 'sequency'], index=index_widget);
 
 
-# ## Linear-feedback shift register (LFSR) sequence
+# ## Linear-feedback shift register (LFSR) sequence -- Maximum-length sequence (MLS)
 
 # In[4]:
 
 
-lfsr = komm.LFSRSequence(0b100101)
-shifts = np.arange(-2*lfsr.length + 1, 2*lfsr.length)
-cyclic_acorr = lfsr.cyclic_autocorrelation(shifts, normalized=True)
+def lfsr_demo(degree):
+    lfsr = komm.LFSRSequence.maximum_length_sequence(degree=degree)
+    length = lfsr.length
+    shifts = np.arange(-2*length + 1, 2*length)
+    _, (ax0, ax1) = plt.subplots(1, 2, figsize=(12, 4))
+    ax0.stem(np.arange(length), lfsr.polar_sequence)
+    ax0.set_title(repr(lfsr))
+    ax0.set_xlabel('$n$')
+    ax0.set_ylabel('$a[n]$')
+    ax0.set_yticks([-1, 0, 1])
+    ax1.stem(shifts, lfsr.cyclic_autocorrelation(shifts, normalized=True))
+    ax1.set_title('Cyclic autocorrelation (normalized)')
+    ax1.set_xlabel('$\\ell$')
+    ax1.set_ylabel('$R[\\ell]$')
+    ax1.set_xticks([-length, 0, length])
+    ax1.set_ylim([-0.5, 1.1])
+    plt.show()
 
-fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(10, 4))
-
-ax0.stem(range(lfsr.length), lfsr.polar_sequence)
-ax0.set_title(repr(lfsr))
-
-ax1.plot(shifts, cyclic_acorr)
-ax1.set_title('Cyclic autocorrelation')
-ax1.grid(True)
-
-plt.show()
+ipywidgets.interact(lfsr_demo, degree=(2, 7));
 
