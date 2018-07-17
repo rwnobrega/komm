d7736808a53c259ed73d89fd5a3749466ffc08c3
Roberto Nobrega <rwnobrega@gmail.com>
2018-07-16 23:05:21 -0300

    Add tests for 'entropy'

---
+++ tests/test_util.py
@@ -0,0 +1,16 @@
+import pytest
+
+import numpy as np
+from komm.util import entropy
+
+
+def test_entropy():
+    assert np.allclose(entropy([0.5, 0.5]), 1.0)
+    assert np.allclose(entropy([1.0, 0.0]), 0.0)
+    assert np.allclose(entropy([0.25, 0.75]), 2.0 - 0.75 * (np.log2(3.0)))
+    assert np.allclose(entropy([1/3, 1/3, 1/3]), np.log2(3))
+    assert np.allclose(entropy([1/3, 1/3, 1/3], base=3.0), 1.0)
+    assert np.allclose(entropy([1/3, 1/3, 1/3], base='e'), np.log(3.0))
+
+def test_mutual_information():
+    pass
