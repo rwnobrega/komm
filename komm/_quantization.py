36a244a805724b3e53d04070ea4f54f117b4274a
Roberto Nobrega <rwnobrega@gmail.com>
2018-07-26 21:37:49 -0300

    Fix uniform quantizer doctest

--- komm/_quantization.py
+++ komm/_quantization.py
@@ -103,8 +103,8 @@ class UniformQuantizer(ScalarQuantizer):
         >>> x = np.linspace(-0.5, 0.5, num=11)
         >>> y = quantizer(x)
         >>> np.vstack([x, y])  #doctest: +NORMALIZE_WHITESPACE
-        array([[-0.5 , -0.4 , -0.3 , -0.2 , -0.1 ,  0.  ,  0.1 ,  0.2 ,  0.3 ,  0.4 ,  0.5 ],
-               [-0.5 , -0.5 , -0.25, -0.25,  0.  ,  0.  ,  0.  ,  0.25,  0.25,  0.5 ,  0.5 ]])
+        array([[-0.5  , -0.4  , -0.3  , -0.2  , -0.1  ,  0.   ,  0.1  ,  0.2  ,  0.3  ,  0.4  ,  0.5  ],
+               [-0.375, -0.375, -0.375, -0.125, -0.125,  0.125,  0.125,  0.125,  0.375,  0.375,  0.625]])
 
         >>> quantizer = komm.UniformQuantizer(num_levels=4, input_peak=1.0, choice='unsigned')
         >>> quantizer.levels
