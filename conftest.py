e77d928a2e01b0165ef96224960f4324814f659b
Roberto Nobrega <rwnobrega@gmail.com>
2018-12-30 02:34:40 -0200

    Add pprint to pytest

--- conftest.py
+++ conftest.py
@@ -1,9 +1,11 @@
 import pytest
 
+import pprint
 import numpy
 import komm
 
 @pytest.fixture(autouse=True)
 def add_namespace(doctest_namespace):
+    doctest_namespace['pprint'] = pprint.pprint
     doctest_namespace['np'] = numpy
     doctest_namespace['komm'] = komm
