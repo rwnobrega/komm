b7112ed89dd6426d7fe0c37f371381fcd19a4c63
Roberto Nobrega <rwnobrega@gmail.com>
2023-05-24 17:25:13 -0300

    Update sphinx settings

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ release = version
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -126,6 +126,8 @@ html_sidebars = {
     ]
 }
 
+# Bibtex
+bibtex_bibfiles = ['refs.bib']
 
 # -------------------------------------------------------------------------
 
