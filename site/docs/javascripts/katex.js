e56c22674074f9aeaf0bc9cbb3424ba5e76d513e
Roberto Nobrega <rwnobrega@gmail.com>
2023-06-13 22:56:11 -0300

    KaTeX changes

--- site/docs/javascripts/katex.js
+++ site/docs/javascripts/katex.js
@@ -1,10 +1,9 @@
-document$.subscribe(({ body }) => {
-    renderMathInElement(body, {
+document.addEventListener("DOMContentLoaded", function () {
+    renderMathInElement(document.body, {
         delimiters: [
-            { left: "$$", right: "$$", display: true },
-            { left: "$", right: "$", display: false },
-            { left: "\\(", right: "\\)", display: false },
-            { left: "\\[", right: "\\]", display: true }
+            { left: '$$', right: '$$', display: true },
+            { left: '$', right: '$', display: false },
         ],
-    })
-})
+        throwOnError: false
+    });
+});
