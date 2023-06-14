0da3499779cf2dafbc8eb8abc8c9792ff9afd287
Roberto Nobrega <rwnobrega@gmail.com>
2023-06-13 23:17:51 -0300

    Deployed ec33f16 with MkDocs version: 1.4.3

--- javascripts/katex.js
+++ javascripts/katex.js
@@ -1,10 +1,15 @@
-document$.subscribe(({ body }) => {
-    renderMathInElement(body, {
+document.addEventListener("DOMContentLoaded", function () {
+    renderMathInElement(document.body, {
         delimiters: [
-            { left: "$$", right: "$$", display: true },
-            { left: "$", right: "$", display: false },
-            { left: "\\(", right: "\\)", display: false },
-            { left: "\\[", right: "\\]", display: true }
+            { left: '$$', right: '$$', display: true },
+            { left: '$', right: '$', display: false },
         ],
-    })
-})
+        macros: {
+            "\\bch": "\\mathrm{BCH}",
+            "\\snr": "\\mathrm{SNR}",
+            "\\Enc": "\\mathrm{Enc}",
+            "\\Dec": "\\mathrm{Dec}",
+        },
+        throwOnError: false
+    });
+});
