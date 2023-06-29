71cd64402f801729645b388457889c06e2087286
Roberto Nobrega <rwnobrega@gmail.com>
2023-06-29 15:35:03 -0300

    Update `ASKModulation`

--- src/komm/_modulation/ASKModulation.py
+++ src/komm/_modulation/ASKModulation.py
@@ -5,11 +5,11 @@ from .Modulation import Modulation
 
 class ASKModulation(Modulation):
     r"""
-    Amplitude-shift keying (ASK) modulation. It is a complex [modulation scheme](/ref/Modulation) in which the points of the constellation $\mathcal{S}$ are *uniformly arranged* in a ray. More precisely,
+    Amplitude-shift keying (ASK) modulation. It is a complex [modulation scheme](/ref/Modulation) in which the points of the constellation symbols are *uniformly arranged* in a ray. More precisely, the rows of the constellation $\mathbf{X}$ are given by
     $$
-        \mathcal{S} = \\{ iA \exp(\mathrm{j}\phi): i \in [0 : M) \\},
+        x_i = iA \exp(\mathrm{j}\phi), \quad i \in [0 : M),
     $$
-    where $M$ is the *order* (a power of $2$), $A$ is the *base amplitude*, and $\phi$ is the *phase offset* of the modulation.  The ASK constellation is depicted below for $M = 4$.
+    where $M$ is the *order* (a power of $2$), $A$ is the *base amplitude*, and $\phi$ is the *phase offset* of the modulation. The ASK constellation is depicted below for $M = 4$.
 
     <figure markdown>
       ![4-ASK constellation.](/figures/ask_4.svg)
@@ -28,20 +28,34 @@ class ASKModulation(Modulation):
 
             phase_offset (Optional[float]): The phase offset $\phi$ of the constellation. The default value is `0.0`.
 
-            labeling (Optional[Array1D[int] | str]): The binary labeling $\mathcal{Q}$ of the modulation. Can be specified either as a 1D-array of integers, in which case must be permutation of $[0 : M)$, or as a string, in which case must be one of `'natural'` or `'reflected'`. The default value is `'reflected'` (Gray code).
+            labeling (Optional[Array1D[int] | str]): The binary labeling of the modulation. Can be specified either as a 2D-array of integers (see [base class](/ref/Modulation) for details), or as a string. In the latter case, the string must be either `'natural'` or `'reflected'`. The default value is `'reflected'`, corresponding to the Gray labeling.
 
         Examples:
 
-            >>> ask = komm.ASKModulation(4, base_amplitude=2.0)
+            The ASK constellation with order $M = 4$, base amplitude $A = 1$, and Gray labeling is depicted below.
+
+            <figure markdown>
+              ![4-ASK constellation with Gray labeling.](/figures/ask_4_gray.svg)
+            </figure>
+
+            >>> ask = komm.ASKModulation(4)
             >>> ask.constellation
-            array([0.+0.j, 2.+0.j, 4.+0.j, 6.+0.j])
+            array([0.+0.j, 1.+0.j, 2.+0.j, 3.+0.j])
             >>> ask.labeling
-            array([0, 1, 3, 2])
+            array([[0, 0],
+                   [1, 0],
+                   [1, 1],
+                   [0, 1]])
             >>> ask.modulate([0, 0, 1, 1, 0, 0, 1, 0, 1, 0])
-            array([0.+0.j, 4.+0.j, 0.+0.j, 2.+0.j, 2.+0.j])
-            >>> ask.demodulate([(0.99+0.3j), (1.01-0.5j), (4.99+0.7j), (5.01-0.9j)])
-            array([0, 0, 1, 0, 1, 1, 0, 1])
+            array([0.+0.j, 2.+0.j, 0.+0.j, 1.+0.j, 1.+0.j])
         """
+        self._constructor_kwargs = {
+            "order": order,
+            "base_amplitude": base_amplitude,
+            "phase_offset": phase_offset,
+            "labeling": labeling,
+        }
+
         constellation = base_amplitude * np.arange(order, dtype=int) * np.exp(1j * phase_offset)
 
         if isinstance(labeling, str):
@@ -56,5 +70,6 @@ class ASKModulation(Modulation):
         self._phase_offset = float(phase_offset)
 
     def __repr__(self):
-        args = "{}, base_amplitude={}, phase_offset={}".format(self._order, self._base_amplitude, self._phase_offset)
-        return "{}({})".format(self.__class__.__name__, args)
+        order, base_amplitude, phase_offset, labeling = self._constructor_kwargs.values()
+        args = f"{order}, base_amplitude={base_amplitude}, phase_offset={phase_offset}, labeling={labeling}"
+        return f"{self.__class__.__name__}({args})"
--- tests/modulation/test_ask_modulation.py
+++ tests/modulation/test_ask_modulation.py
@@ -1,18 +1,68 @@
 import numpy as np
+import pytest
 
 import komm
 
 
-def test_ask_modulation():
-    ask2 = komm.ASKModulation(2)
-    ask4 = komm.ASKModulation(4)
-    ask8 = komm.ASKModulation(8)
+@pytest.mark.parametrize(
+    "order, bits_per_symbol, constellation, energy_per_symbol, energy_per_bit, symbol_mean",
+    [
+        (2, 1, [0.0, 1.0], 0.5, 0.5, 0.5),
+        (4, 2, [0.0, 1.0, 2.0, 3.0], 3.5, 1.75, 1.5),
+        (8, 3, [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0], 17.5, 35 / 6, 3.5),
+    ],
+)
+def test_ask_modulation_1(order, constellation, bits_per_symbol, energy_per_symbol, energy_per_bit, symbol_mean):
+    ask = komm.ASKModulation(order)
+    assert ask.order == order
+    assert ask.bits_per_symbol == bits_per_symbol
+    assert np.allclose(ask.constellation, constellation)
+    assert np.allclose(ask.energy_per_symbol, energy_per_symbol)
+    assert np.allclose(ask.energy_per_bit, energy_per_bit)
+    assert np.allclose(ask.symbol_mean, symbol_mean)
+    assert np.allclose(ask.minimum_distance, 1.0)
 
-    assert np.allclose(ask2.constellation, [0.0, 1.0])
-    assert np.allclose(ask4.constellation, [0.0, 1.0, 2.0, 3.0])
-    assert np.allclose(ask8.constellation, [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0])
 
-    x = [-0.5, 0.25, 0.4, 0.65, 2.1, 10.0]
-    assert np.allclose(ask2.demodulate(x), ask2.symbols_to_bits([0, 0, 0, 1, 1, 1]))
-    assert np.allclose(ask4.demodulate(x), ask4.symbols_to_bits([0, 0, 0, 1, 2, 3]))
-    assert np.allclose(ask8.demodulate(x), ask8.symbols_to_bits([0, 0, 0, 1, 2, 7]))
+@pytest.mark.parametrize(
+    "base_amplitude, phase_offset, constellation",
+    [
+        (1.0, 0.0, [0.0, 1.0, 2.0, 3.0]),
+        (2.0, 0.0, [0.0, 2.0, 4.0, 6.0]),
+        (1.0, np.pi / 4.0, np.array([0, 1 + 1j, 2 + 2j, 3 + 3j]) / np.sqrt(2)),
+        (0.5, np.pi, [0, -0.5, -1, -1.5]),
+        (2.5, np.pi / 2.0, [0, 2.5j, 5j, 7.5j]),
+    ],
+)
+def test_ask_modulation_2(base_amplitude, phase_offset, constellation):
+    ask4 = komm.ASKModulation(4, base_amplitude=base_amplitude, phase_offset=phase_offset)
+    assert np.allclose(ask4.constellation, constellation)
+    assert np.allclose(ask4.energy_per_symbol, 3.5 * base_amplitude**2)
+    assert np.allclose(ask4.energy_per_bit, 1.75 * base_amplitude**2)
+    assert np.allclose(ask4.symbol_mean, 1.5 * base_amplitude * np.exp(1j * phase_offset))
+    assert np.allclose(ask4.minimum_distance, base_amplitude)
+
+
+@pytest.mark.parametrize("order", [2, 4, 8])
+@pytest.mark.parametrize("labeling", ["natural", "reflected"])
+def test_ask_modulation_3(order, labeling):
+    # Test hard demodulation
+    ask = komm.PAModulation(order, labeling=labeling)
+    m = ask.bits_per_symbol
+    bits = np.random.randint(0, 2, size=100 * m, dtype=int)
+    x = ask.modulate(bits)
+    y = x
+    assert np.allclose(ask.demodulate(y, decision_method="hard"), bits)
+
+
+@pytest.mark.parametrize(
+    "order, demodulated",
+    [
+        (2, [0, 0, 0, 1, 1, 1]),
+        (4, [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1]),
+        (8, [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1]),
+    ],
+)
+def test_ask_modulation_4(order, demodulated):
+    ask = komm.ASKModulation(order)
+    y = [-0.5, 0.25, 0.4, 0.65, 2.1, 10.0]
+    assert np.allclose(ask.demodulate(y), demodulated)
