495612e117a4350aa055e6e27b54f3155f72ffd7
Roberto Nobrega <rwnobrega@gmail.com>
2023-07-02 00:44:29 -0300

    Update `QAModulation`

--- src/komm/_modulation/QAModulation.py
+++ src/komm/_modulation/QAModulation.py
@@ -1,15 +1,21 @@
 import numpy as np
 
 from .Modulation import Modulation
+from .PAModulation import PAModulation
 
 
 class QAModulation(Modulation):
     r"""
-    Quadrature-amplitude modulation (QAM). It is a complex [modulation scheme](/ref/Modulation) in which the constellation $\mathcal{S}$ is given as a Cartesian product of two [PAM](/ref/PAModulation) constellations, namely, the *in-phase constellation*, and the *quadrature constellation*. More precisely,
+    Quadrature-amplitude modulation (QAM). It is a complex [modulation scheme](/ref/Modulation) in which the constellation is given as a Cartesian product of two [PAM](/ref/PAModulation) constellations, namely, the *in-phase constellation*, and the *quadrature constellation*. More precisely, the $i$-th constellation symbol is given by
     $$
-        \mathcal{S} = \\{ [\pm(2i_\mathrm{I} + 1)A_\mathrm{I} \pm \mathrm{j}(2i_\mathrm{Q} + 1)A_\mathrm{Q}] \exp(\mathrm{j}\phi) : i_\mathrm{I} \in [0 : M_\mathrm{I}), i_\mathrm{Q} \in [0 : M_\mathrm{Q}) \\},
+        \begin{aligned}
+            x_i = \left[ A_\mathrm{I} \left( 2i_\mathrm{I} - M_\mathrm{I} + 1 \right) + \mathrm{j} A_\mathrm{Q} \left( 2i_\mathrm{Q} - M_\mathrm{Q} + 1 \right) \right] \exp(\mathrm{j}\phi), \quad
+                &  i \in [0 : M), \\\\
+                & i_\mathrm{I} = i \bmod M_\mathrm{I}, \\\\
+                & i_\mathrm{Q} = \lfloor i / M_\mathrm{I} \rfloor,
+        \end{aligned}
     $$
-    where $M_\mathrm{I}$ and $M_\mathrm{Q}$ are the *orders* (powers of $2$), and $A_\mathrm{I}$ and $A_\mathrm{Q}$ are the *base amplitudes* of the in-phase and quadrature constellations, respectively. Also, $\phi$ is the *phase offset*. The size of the resulting complex-valued constellation is $M = M_\mathrm{I} M_\mathrm{Q}$, a power of $2$. The QAM constellation is depicted below for $(M_\mathrm{I}, M_\mathrm{Q}) = (4, 4)$ with $A_\mathrm{I} = A_\mathrm{Q} = A$, and for $(M_\mathrm{I}, M_\mathrm{Q}) = (4, 2)$ with $A_\mathrm{I} = A$ and $A_\mathrm{Q} = 2A$; in both cases, $\phi = 0$.
+    where $M_\mathrm{I}$ and $M_\mathrm{Q}$ are the *orders* (powers of $2$), and $A_\mathrm{I}$ and $A_\mathrm{Q}$ are the *base amplitudes* of the in-phase and quadrature constellations, respectively. Also, $\phi$ is the *phase offset*. The order of the resulting complex-valued constellation is $M = M_\mathrm{I} M_\mathrm{Q}$, a power of $2$. The QAM constellation is depicted below for $(M_\mathrm{I}, M_\mathrm{Q}) = (4, 4)$ with ($A_\mathrm{I}, A_\mathrm{Q}) = (A, A)$; and for $(M_\mathrm{I}, M_\mathrm{Q}) = (4, 2)$ with $(A_\mathrm{I}, A_\mathrm{Q}) = (A, 2A)$; in both cases, $\phi = 0$.
 
     <div class="centered" markdown>
       <span>
@@ -29,13 +35,19 @@ class QAModulation(Modulation):
 
             orders (Tuple(int, int) | int): A tuple $(M_\mathrm{I}, M_\mathrm{Q})$ with the orders of the in-phase and quadrature constellations, respectively; both $M_\mathrm{I}$ and $M_\mathrm{Q}$ must be powers of $2$. If specified as a single integer $M$, then it is assumed that $M_\mathrm{I} = M_\mathrm{Q} = \sqrt{M}$; in this case, $M$ must be an square power of $2$.
 
-            base_amplitudes (Optional[Tuple(float, float) | float]): A tuple $(A_\mathrm{I}, A_\mathrm{Q})$ with the base amplitudes of the in-phase and quadrature constellations, respectively.  If specified as a single float $A$, then it is assumed that $A_\mathrm{I} = A_\mathrm{Q} = A$. The default value is $1.0$.
+            base_amplitudes (Optional[Tuple(float, float) | float]): A tuple $(A_\mathrm{I}, A_\mathrm{Q})$ with the base amplitudes of the in-phase and quadrature constellations, respectively. If specified as a single float $A$, then it is assumed that $A_\mathrm{I} = A_\mathrm{Q} = A$. The default value is $1.0$.
 
             phase_offset (Optional[float]): The phase offset $\phi$ of the constellation. The default value is `0.0`.
 
-            labeling (Optional[Array1D[int] | str]): The binary labeling $\mathcal{Q}$ of the modulation. Can be specified either as a 1D-array of integers, in which case must be permutation of $[0 : M)$, or as a string, in which case must be one of `'natural'` or `'reflected_2d'`. The default value is `'reflected_2d'` (Gray code).
+            labeling (Optional[Array1D[int] | str]): The binary labeling of the modulation. Can be specified either as a 2D-array of integers (see [base class](/ref/Modulation) for details), or as a string. In the latter case, the string must be either `'natural_2d'` or `'reflected_2d'`. The default value is `'reflected_2d'`, corresponding to the Gray labeling.
 
         Examples:
+
+            The square $16$-QAM modulation with $(M_\mathrm{I}, M_\mathrm{Q}) = (4, 4)$ and $(A_\mathrm{I}, A_\mathrm{Q}) = (1, 1)$, and Gray labeling is depicted below.
+
+            <figure markdown>
+              ![16-QAM modulation with Gray labeling.](/figures/qam_16_gray.svg)
+            </figure>
 
             >>> qam = komm.QAModulation(16)
             >>> qam.constellation  #doctest: +NORMALIZE_WHITESPACE
@@ -43,16 +55,27 @@ class QAModulation(Modulation):
                    -3.-1.j, -1.-1.j,  1.-1.j,  3.-1.j,
                    -3.+1.j, -1.+1.j,  1.+1.j,  3.+1.j,
                    -3.+3.j, -1.+3.j,  1.+3.j,  3.+3.j])
-            >>> qam.labeling
-            array([ 0,  1,  3,  2,  4,  5,  7,  6, 12, 13, 15, 14,  8,  9, 11, 10])
+            >>> qam.labeling   #doctest: +NORMALIZE_WHITESPACE
+            array([[0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0],
+                   [0, 0, 1, 0], [1, 0, 1, 0], [1, 1, 1, 0], [0, 1, 1, 0],
+                   [0, 0, 1, 1], [1, 0, 1, 1], [1, 1, 1, 1], [0, 1, 1, 1],
+                   [0, 0, 0, 1], [1, 0, 0, 1], [1, 1, 0, 1], [0, 1, 0, 1]])
             >>> qam.modulate([0, 0, 1, 1, 0, 0, 1, 0])
             array([-3.+1.j, -3.-1.j])
 
+            The rectangular $8$-QAM modulation with $(M_\mathrm{I}, M_\mathrm{Q}) = (4, 2)$ and $(A_\mathrm{I}, A_\mathrm{Q}) = (1, 2)$, and Gray labeling is depicted below.
+
+            <figure markdown>
+              ![8-QAM modulation with Gray labeling.](/figures/qam_8_gray.svg)
+            </figure>
+
             >>> qam = komm.QAModulation(orders=(4, 2), base_amplitudes=(1.0, 2.0))
             >>> qam.constellation  #doctest: +NORMALIZE_WHITESPACE
-            array([-3.-2.j, -1.-2.j,  1.-2.j,  3.-2.j, -3.+2.j, -1.+2.j,  1.+2.j,  3.+2.j])
-            >>> qam.labeling
-            array([0, 1, 3, 2, 4, 5, 7, 6])
+            array([-3.-2.j, -1.-2.j,  1.-2.j,  3.-2.j,
+                   -3.+2.j, -1.+2.j,  1.+2.j,  3.+2.j])
+            >>> qam.labeling  #doctest: +NORMALIZE_WHITESPACE
+            array([[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0],
+                   [0, 0, 1], [1, 0, 1], [1, 1, 1], [0, 1, 1]])
             >>> qam.modulate([0, 0, 1, 1, 0, 0, 1, 0, 1])
             array([-3.+2.j, -1.-2.j, -1.+2.j])
         """
@@ -70,19 +93,13 @@ class QAModulation(Modulation):
             base_amplitude_I = base_amplitude_Q = float(base_amplitudes)
             self._base_amplitudes = base_amplitude_I
 
-        constellation_I = base_amplitude_I * np.arange(-order_I + 1, order_I, step=2, dtype=int)
-        constellation_Q = base_amplitude_Q * np.arange(-order_Q + 1, order_Q, step=2, dtype=int)
-        constellation = (constellation_I + 1j * constellation_Q[np.newaxis].T).flatten() * np.exp(1j * phase_offset)
+        constellation = self._qam_constellation(order_I, order_Q, base_amplitude_I, base_amplitude_Q, phase_offset)
 
         if isinstance(labeling, str):
-            if labeling == "natural":
-                labeling = Modulation._labeling_natural(order_I * order_Q)
-            elif labeling == "reflected_2d":
-                labeling = Modulation._labeling_reflected_2d(order_I, order_Q)
+            if labeling in ["natural_2d", "reflected_2d"]:
+                labeling = getattr(Modulation, "_labeling_" + labeling)(order_I, order_Q)
             else:
-                raise ValueError(
-                    "Only 'natural' or 'reflected_2d' are supported for {}".format(self.__class__.__name__)
-                )
+                raise ValueError(f"Only 'natural_2d' or 'reflected_2d' are supported for {self.__class__.__name__}")
 
         super().__init__(constellation, labeling)
 
@@ -93,3 +110,9 @@ class QAModulation(Modulation):
     def __repr__(self):
         args = "{}, base_amplitudes={}, phase_offset={}".format(self._orders, self._base_amplitudes, self._phase_offset)
         return "{}({})".format(self.__class__.__name__, args)
+
+    @staticmethod
+    def _qam_constellation(order_I, order_Q, base_amplitude_I, base_amplitude_Q, phase_offset):
+        constellation_I = PAModulation._pam_constellation(order_I, base_amplitude_I)
+        constellation_Q = PAModulation._pam_constellation(order_Q, base_amplitude_Q)
+        return (constellation_I + 1j * constellation_Q[np.newaxis].T).flatten() * np.exp(1j * phase_offset)
--- tests/modulation/test_qam_modulation.py
+++ tests/modulation/test_qam_modulation.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pytest
 
 import komm
+from komm._util.matrices import cartesian_product
 
 
-def test_qam_modulation():
+def test_qam_modulation_1():
     qam4 = komm.QAModulation(4)
     qam8 = komm.QAModulation((4, 2))
     qam16 = komm.QAModulation(16)
@@ -50,3 +52,31 @@ def test_qam_modulation():
             3.0 + 3.0j,
         ],
     )
+
+
+@pytest.mark.parametrize(
+    "orders, base_amplitudes",
+    [
+        ((4, 2), (1, 2)),
+        ((4, 2), (3, 1)),
+        ((2, 4), (10, 20)),
+        ((2, 4), (15, 12)),
+        ((4, 4), (3, 7)),
+    ],
+)
+def test_qam_modulation_2(orders, base_amplitudes):
+    (M_I, M_Q) = orders
+    (A_I, A_Q) = base_amplitudes
+    qam = komm.QAModulation(orders, base_amplitudes=base_amplitudes)
+
+    pam_I = komm.PAModulation(M_I, base_amplitude=A_I)
+    pam_Q = komm.PAModulation(M_Q, base_amplitude=A_Q)
+    qam_real_2d = cartesian_product(pam_I.constellation, pam_Q.constellation)
+    qam_complex_1d = qam_real_2d[:, 0] + 1j * qam_real_2d[:, 1]
+    assert np.allclose(qam_complex_1d, qam.constellation)
+
+    qam_doc_formula = []
+    for i in range(M_I * M_Q):
+        i_I, i_Q = i % M_I, i // M_I
+        qam_doc_formula.append(A_I * (2 * i_I - M_I + 1) + 1j * A_Q * (2 * i_Q - M_Q + 1))
+    assert np.allclose(qam_doc_formula, qam.constellation)
