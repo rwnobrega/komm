16a8a1a8e1fa10c551be51ef30f51649082ac9f3
Roberto Nobrega <rwnobrega@gmail.com>
2023-07-23 16:40:02 -0300

    Update `ConvolutionalStreamEncoder`

--- src/komm/_error_control_convolutional/ConvolutionalStreamEncoder.py
+++ src/komm/_error_control_convolutional/ConvolutionalStreamEncoder.py
@@ -1,41 +1,48 @@
+import numpy as np
+import numpy.typing as npt
+from attrs import define, field
+
 from .._util import pack, unpack
+from .ConvolutionalCode import ConvolutionalCode
 
 
+@define
 class ConvolutionalStreamEncoder:
     r"""
     Convolutional stream encoder. Encode a bit stream using a given [convolutional code](/ref/ConvolutionalCode). The internal state of the encoder is maintained across each call.
 
-    To invoke the encoder, call the object giving the input signal as parameter (see example in the constructor below).
+    Attributes:
+
+        convolutional_code: The convolutional code.
+        state: The current state of the encoder. The default value is `0`.
+
+    Parameters: Input:
+
+        in0 (Array1D[int]): The bit sequence to be encoded.
+
+    Parameters: Output:
+
+        out0 (Array1D[int]): The encoded bit sequence.
+
+    Examples:
+
+        >>> convolutional_code = komm.ConvolutionalCode([[0o7, 0o5]])
+        >>> convolutional_encoder = komm.ConvolutionalStreamEncoder(convolutional_code)
+        >>> convolutional_encoder([1, 1, 1, 1])
+        array([1, 1, 0, 1, 1, 0, 1, 0])
+        >>> convolutional_encoder([1, 1, 1, 1])
+        array([1, 0, 1, 0, 1, 0, 1, 0])
     """
+    convolutional_code: ConvolutionalCode
+    state: int = field(default=0)
 
-    def __init__(self, convolutional_code, initial_state=0):
-        r"""
-        Constructor for the class.
-
-        Parameters:
-
-            convolutional_code (ConvolutionalCode): The convolutional code.
-
-            initial_state (Optional[int]): Initial state of the encoder. The default value is `0`.
-
-        Examples:
-
-            >>> convolutional_code = komm.ConvolutionalCode([[0o7, 0o5]])
-            >>> convolutional_encoder = komm.ConvolutionalStreamEncoder(convolutional_code)
-            >>> convolutional_encoder([1, 1, 1, 1])
-            array([1, 1, 0, 1, 1, 0, 1, 0])
-            >>> convolutional_encoder([1, 1, 1, 1])
-            array([1, 0, 1, 0, 1, 0, 1, 0])
-        """
-        self._convolutional_code = convolutional_code
-        self._state = int(initial_state)
-
-    def __call__(self, inp):
-        n, k = self._convolutional_code.num_output_bits, self._convolutional_code.num_input_bits
-
-        output_sequence, self._state = self._convolutional_code.finite_state_machine.process(
-            input_sequence=pack(inp, width=k), initial_state=self._state
+    def __call__(self, in0: npt.ArrayLike) -> np.ndarray:
+        n, k, fsm = (
+            self.convolutional_code.num_output_bits,
+            self.convolutional_code.num_input_bits,
+            self.convolutional_code.finite_state_machine,
         )
+        output_sequence, self.state = fsm.process(input_sequence=pack(in0, width=k), initial_state=self.state)
 
-        outp = unpack(output_sequence, width=n)
-        return outp
+        out0 = unpack(output_sequence, width=n)
+        return out0
