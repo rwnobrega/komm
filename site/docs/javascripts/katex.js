768d75b0d8bd809539a1e1d643ea420e9bae6603
Roberto Nobrega <rwnobrega@gmail.com>
2024-11-01 22:33:29 -0300

    Format  `css` and `js` with Prettier

--- site/docs/javascripts/katex.js
+++ site/docs/javascripts/katex.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    renderMathInElement(document.body, {
-        delimiters: [
-            { left: '$$', right: '$$', display: true },
-            { left: '$', right: '$', display: false },
-        ],
-        macros: {
-            "\\bch": "\\mathrm{BCH}",
-            "\\snr": "\\mathrm{SNR}",
-            "\\Enc": "\\mathrm{Enc}",
-            "\\Dec": "\\mathrm{Dec}",
-        },
-        throwOnError: false
-    });
+  renderMathInElement(document.body, {
+    delimiters: [
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false },
+    ],
+    macros: {
+      "\\bch": "\\mathrm{BCH}",
+      "\\snr": "\\mathrm{SNR}",
+      "\\Enc": "\\mathrm{Enc}",
+      "\\Dec": "\\mathrm{Dec}",
+    },
+    throwOnError: false,
+  });
 });
