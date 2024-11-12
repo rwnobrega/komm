8ca90269307f83c3e83a073259074c01d59d1fb2
Roberto Nobrega <rwnobrega@gmail.com>
2024-11-12 14:00:37 -0300

    Update `__all__` in `_channels`

--- src/komm/_channels/__init__.py
+++ src/komm/_channels/__init__.py
@@ -3,3 +3,11 @@ from .BinaryErasureChannel import BinaryErasureChannel
 from .BinarySymmetricChannel import BinarySymmetricChannel
 from .DiscreteMemorylessChannel import DiscreteMemorylessChannel
 from .ZChannel import ZChannel
+
+__all__ = [
+    "AWGNChannel",
+    "BinaryErasureChannel",
+    "BinarySymmetricChannel",
+    "DiscreteMemorylessChannel",
+    "ZChannel",
+]
