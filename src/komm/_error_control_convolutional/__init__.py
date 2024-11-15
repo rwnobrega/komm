69debbe3a58da0ea2e0ed191a3d8a4cc331af91f
Roberto Nobrega <rwnobrega@gmail.com>
2024-11-15 12:56:03 -0300

    Update `__all__` in `_error_control_convolutional`

--- src/komm/_error_control_convolutional/__init__.py
+++ src/komm/_error_control_convolutional/__init__.py
@@ -1,3 +1,9 @@
 from .ConvolutionalCode import ConvolutionalCode
 from .ConvolutionalStreamDecoder import ConvolutionalStreamDecoder
 from .ConvolutionalStreamEncoder import ConvolutionalStreamEncoder
+
+__all__ = [
+    "ConvolutionalCode",
+    "ConvolutionalStreamDecoder",
+    "ConvolutionalStreamEncoder",
+]
