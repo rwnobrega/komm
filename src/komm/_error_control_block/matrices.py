49d64053e9bc697aa263092b14ebdfb990698e04
Roberto Nobrega <rwnobrega@gmail.com>
2024-11-14 23:44:14 -0300

    Add type hints for matrix functions in error control block

--- src/komm/_error_control_block/matrices.py
+++ src/komm/_error_control_block/matrices.py
@@ -1,15 +1,18 @@
 import itertools as it
 
 import numpy as np
+import numpy.typing as npt
+
+BinaryMatrix = npt.NDArray[np.int_]
 
 
-def extended_parity_submatrix(parity_submatrix):
+def extended_parity_submatrix(parity_submatrix: BinaryMatrix) -> BinaryMatrix:
     last_column = (1 + np.sum(parity_submatrix, axis=1)) % 2
     extended_parity_submatrix = np.hstack([parity_submatrix, last_column[np.newaxis].T])
     return extended_parity_submatrix
 
 
-def hamming_parity_submatrix(m, extended=False):
+def hamming_parity_submatrix(m: int, extended: bool = False) -> BinaryMatrix:
     parity_submatrix = np.zeros((2**m - m - 1, m), dtype=int)
     i = 0
     for w in range(2, m + 1):
@@ -21,7 +24,7 @@ def hamming_parity_submatrix(m, extended=False):
     return parity_submatrix
 
 
-def golay_parity_submatrix(extended=False):
+def golay_parity_submatrix(extended: bool = False) -> BinaryMatrix:
     parity_submatrix = np.array(
         [
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
