898ba092e9737b827d557d728d8b83cf82b55908
Roberto Nobrega <rwnobrega@gmail.com>
2024-11-21 22:18:07 -0300

    Convert `FiniteStateMachine` to attrs and improve typing

--- src/komm/_finite_state_machine/FiniteStateMachine.py
+++ src/komm/_finite_state_machine/FiniteStateMachine.py
@@ -1,6 +1,20 @@
+from functools import cached_property
+from typing import Callable, Optional, Sequence, TypedDict
+
 import numpy as np
+import numpy.typing as npt
+from attrs import field, frozen
 
 
+class MetricMemory(TypedDict):
+    paths: npt.NDArray[np.int_]
+    metrics: npt.NDArray[np.float64]
+
+
+MetricFunction = Callable[[int, int], float]
+
+
+@frozen
 class FiniteStateMachine:
     r"""
     Finite-state machine (Mealy machine). It is defined by a *set of states* $\mathcal{S}$, an *input alphabet* $\mathcal{X}$, an *output alphabet* $\mathcal{Y}$, and a *transition function* $T : \mathcal{S} \times \mathcal{X} \to \mathcal{S} \times \mathcal{Y}$. Here, for simplicity, the set of states, the input alphabet, and the output alphabet are always taken as $\mathcal{S} = \\{ 0, 1, \ldots, |\mathcal{S}| - 1 \\}$, $\mathcal{X} = \\{ 0, 1, \ldots, |\mathcal{X}| - 1 \\}$, and $\mathcal{Y} = \\{ 0, 1, \ldots, |\mathcal{Y}| - 1 \\}$, respectively.
@@ -23,77 +37,58 @@ class FiniteStateMachine:
     | $2$   | $1$   | $1$        | $0$    |
     | $3$   | $0$   | $2$        | $2$    |
     | $3$   | $1$   | $3$        | $1$    |
+
+    Attributes:
+        next_states (Array2D[int]): The matrix of next states of the machine, of shape $|\mathcal{S}| \times |\mathcal{X}|$. The element in row $s$ and column $x$ should be the next state of the machine (an element in $\mathcal{S}$), given that the current state is $s \in \mathcal{S}$ and the input is $x \in \mathcal{X}$.
+
+        outputs (Array2D[int]): The matrix of outputs of the machine, of shape $|\mathcal{S}| \times |\mathcal{X}|$. The element in row $s$ and column $x$ should be the output of the machine (an element in $\mathcal{Y}$), given that the current state is $s \in \mathcal{S}$ and the input is $x \in \mathcal{X}$.
+
+    Examples:
+        >>> fsm = komm.FiniteStateMachine(next_states=[[0,1], [2,3], [0,1], [2,3]], outputs=[[0,3], [1,2], [3,0], [2,1]])
     """
 
-    def __init__(self, next_states, outputs):
-        r"""
-        Constructor for the class.
+    next_states: npt.NDArray[np.int_] = field(
+        converter=np.asarray, repr=lambda x: x.tolist()
+    )
+    outputs: npt.NDArray[np.int_] = field(
+        converter=np.asarray, repr=lambda x: x.tolist()
+    )
+    _input_edges: npt.NDArray[np.int_] = field(init=False, repr=False)
+    _output_edges: npt.NDArray[np.int_] = field(init=False, repr=False)
 
-        Parameters:
-            next_states (Array2D[int]): The matrix of next states of the machine, of shape $|\mathcal{S}| \times |\mathcal{X}|$. The element in row $s$ and column $x$ should be the next state of the machine (an element in $\mathcal{S}$), given that the current state is $s \in \mathcal{S}$ and the input is $x \in \mathcal{X}$.
+    def __attrs_post_init__(self) -> None:
+        input_edges = np.full((self.num_states, self.num_states), fill_value=-1)
+        output_edges = np.full((self.num_states, self.num_states), fill_value=-1)
+        for state_from in range(self.num_states):
+            for x, state_to in enumerate(self.next_states[state_from, :]):
+                input_edges[state_from, state_to] = x
+                output_edges[state_from, state_to] = self.outputs[state_from, x]
+        object.__setattr__(self, "_input_edges", input_edges)
+        object.__setattr__(self, "_output_edges", output_edges)
 
-            outputs (Array2D[int]): The matrix of outputs of the machine, of shape $|\mathcal{S}| \times |\mathcal{X}|$. The element in row $s$ and column $x$ should be the output of the machine (an element in $\mathcal{Y}$), given that the current state is $s \in \mathcal{S}$ and the input is $x \in \mathcal{X}$.
-
-        Examples:
-            >>> fsm = komm.FiniteStateMachine(next_states=[[0,1], [2,3], [0,1], [2,3]], outputs=[[0,3], [1,2], [3,0], [2,1]])
-        """
-        self._next_states = np.array(next_states, dtype=int)
-        self._outputs = np.array(outputs, dtype=int)
-        self._num_states, self._num_input_symbols = self._next_states.shape
-        self._num_output_symbols = np.amax(self._outputs)
-
-        self._input_edges = np.full((self._num_states, self._num_states), fill_value=-1)
-        self._output_edges = np.full(
-            (self._num_states, self._num_states), fill_value=-1
-        )
-        for state_from in range(self._num_states):
-            for x, state_to in enumerate(self._next_states[state_from, :]):
-                self._input_edges[state_from, state_to] = x
-                self._output_edges[state_from, state_to] = self._outputs[state_from, x]
-
-    def __repr__(self):
-        args = "next_states={}, outputs={}".format(
-            self._next_states.tolist(), self._outputs.tolist()
-        )
-        return "{}({})".format(self.__class__.__name__, args)
-
-    @property
-    def num_states(self):
+    @cached_property
+    def num_states(self) -> int:
         r"""
         The number of states of the machine.
         """
-        return self._num_states
+        return self.next_states.shape[0]
 
-    @property
-    def num_input_symbols(self):
+    @cached_property
+    def num_input_symbols(self) -> int:
         r"""
         The size (cardinality) of the input alphabet $\mathcal{X}$.
         """
-        return self._num_input_symbols
+        return self.next_states.shape[1]
 
-    @property
-    def num_output_symbols(self):
+    @cached_property
+    def num_output_symbols(self) -> int:
         r"""
         The size (cardinality) of the output alphabet $\mathcal{Y}$.
         """
-        return self._num_output_symbols
+        return int(np.amax(self.outputs))
 
     @property
-    def next_states(self):
-        r"""
-        The matrix of next states of the machine. It has shape $|\mathcal{S}| \times |\mathcal{X}|$. The element in row $s$ and column $x$ is the next state of the machine (an element in $\mathcal{S}$), given that the current state is $s \in \mathcal{S}$ and the input is $x \in \mathcal{X}$.
-        """
-        return self._next_states
-
-    @property
-    def outputs(self):
-        r"""
-        The matrix of outputs of the machine. It has shape $|\mathcal{S}| \times |\mathcal{X}|$. The element in row $s$ and column $x$ is the output of the machine (an element in $\mathcal{Y}$), given that the current state is $s \in \mathcal{S}$ and the input is $x \in \mathcal{X}$.
-        """
-        return self._outputs
-
-    @property
-    def input_edges(self):
+    def input_edges(self) -> npt.NDArray[np.int_]:
         r"""
         The matrix of input edges of the machine. It has shape $|\mathcal{S}| \times |\mathcal{S}|$. If there is an edge from $s_0 \in \mathcal{S}$ to $s_1 \in \mathcal{S}$, then the element in row $s_0$ and column $s_1$ is the input associated with that edge (an element of $\mathcal{X}$); if there is no such edge, then the element is $-1$.
 
@@ -108,7 +103,7 @@ class FiniteStateMachine:
         return self._input_edges
 
     @property
-    def output_edges(self):
+    def output_edges(self) -> npt.NDArray[np.int_]:
         r"""
         The matrix of output edges of the machine. It has shape $|\mathcal{S}| \times |\mathcal{S}|$. If there is an edge from $s_0 \in \mathcal{S}$ to $s_1 \in \mathcal{S}$, then the element in row $s_0$ and column $s_1$ is the output associated with that edge (an element of $\mathcal{Y}$); if there is no such edge, then the element is $-1$.
 
@@ -122,7 +117,11 @@ class FiniteStateMachine:
         """
         return self._output_edges
 
-    def process(self, input_sequence, initial_state):
+    def process(
+        self,
+        input_sequence: Sequence[int],
+        initial_state: int,
+    ) -> tuple[npt.NDArray[np.int_], int]:
         r"""
         Returns the output sequence corresponding to a given input sequence. It assumes the machine starts at a given initial state $s_\mathrm{i}$. The input sequence and the output sequence are denoted by $\mathbf{x} = (x_0, x_1, \ldots, x_{L-1}) \in \mathcal{X}^L$ and $\mathbf{y} = (y_0, y_1, \ldots, y_{L-1}) \in \mathcal{Y}^L$, respectively.
 
@@ -148,13 +147,18 @@ class FiniteStateMachine:
         output_sequence = np.empty_like(input_sequence, dtype=int)
         s = initial_state
         for t, x in np.ndenumerate(input_sequence):
-            y = self._outputs[s, x]
-            s = self._next_states[s, x]
+            y = self.outputs[s, x]
+            s = self.next_states[s, x]
             output_sequence[t] = y
         final_state = s
         return output_sequence, final_state
 
-    def viterbi(self, observed_sequence, metric_function, initial_metrics=None):
+    def viterbi(
+        self,
+        observed_sequence: Sequence[int],
+        metric_function: MetricFunction,
+        initial_metrics: Optional[npt.ArrayLike] = None,
+    ) -> tuple[npt.NDArray[np.int_], npt.NDArray[np.float64]]:
         r"""
         Applies the Viterbi algorithm on a given observed sequence. The Viterbi algorithm finds the most probable input sequence $\hat{\mathbf{x}}(s) \in \mathcal{X}^L$ ending in state $s$, for all $s \in \mathcal{S}$, given an observed sequence $\mathbf{z} \in \mathcal{Z}^L$. It is assumed uniform input priors. See <cite>LC04, Sec. 12.1</cite>.
 
@@ -170,7 +174,7 @@ class FiniteStateMachine:
 
             final_metrics (Array1D[float]): The final metrics for each state. It is a 1D-array of length $|\mathcal{S}|$.
         """
-        L, num_states = len(observed_sequence), self._num_states
+        L, num_states = len(observed_sequence), self.num_states
         choices = np.empty((L, num_states), dtype=int)
         metrics = np.full((L + 1, num_states), fill_value=np.inf)
         if initial_metrics is None:
@@ -179,7 +183,7 @@ class FiniteStateMachine:
             metrics[0, :] = initial_metrics
         for t, z in enumerate(observed_sequence):
             for s0 in range(num_states):
-                for s1, y in zip(self._next_states[s0], self._outputs[s0]):
+                for s1, y in zip(self.next_states[s0], self.outputs[s0]):
                     candidate_metrics = metrics[t, s0] + metric_function(y, z)
                     if candidate_metrics < metrics[t + 1, s1]:
                         metrics[t + 1, s1] = candidate_metrics
@@ -191,12 +195,17 @@ class FiniteStateMachine:
             s1 = final_state
             for t in reversed(range(L)):
                 s0 = choices[t, s1]
-                input_sequences_hat[t, final_state] = self._input_edges[s0, s1]
+                input_sequences_hat[t, final_state] = self.input_edges[s0, s1]
                 s1 = s0
 
         return input_sequences_hat, metrics[L, :]
 
-    def viterbi_streaming(self, observed_sequence, metric_function, memory):
+    def viterbi_streaming(
+        self,
+        observed_sequence: Sequence[int],
+        metric_function: MetricFunction,
+        memory: MetricMemory,
+    ) -> npt.NDArray[np.int_]:
         r"""
         Applies the streaming version of the Viterbi algorithm on a given observed sequence. The path memory (or traceback length) is denoted by $\tau$. It chooses the survivor with best metric and selects the information block on this path. See <cite>LC04, Sec. 12.3</cite>.
 
@@ -210,13 +219,13 @@ class FiniteStateMachine:
         Returns:
             input_sequence_hat (Array1D[int]): The most probable input sequence $\hat{\mathbf{x}} \in \mathcal{X}^L$
         """
-        num_states = self._num_states
+        num_states = self.num_states
         input_sequences_hat = np.empty(len(observed_sequence), dtype=int)
         for t, z in enumerate(observed_sequence):
             new_metrics = np.full(num_states, fill_value=np.inf)
             choices = np.zeros(num_states, dtype=int)
             for s0 in range(num_states):
-                for s1, y in zip(self._next_states[s0], self._outputs[s0]):
+                for s1, y in zip(self.next_states[s0], self.outputs[s0]):
                     candidate_metric = memory["metrics"][s0, -1] + metric_function(y, z)
                     if candidate_metric < new_metrics[s1]:
                         new_metrics[s1] = candidate_metric
@@ -224,7 +233,7 @@ class FiniteStateMachine:
 
             s_star = np.argmin(new_metrics)
             s0, s1 = memory["paths"][s_star, :2]
-            input_sequences_hat[t] = self._input_edges[s0, s1]
+            input_sequences_hat[t] = self.input_edges[s0, s1]
 
             memory["metrics"] = np.roll(memory["metrics"], shift=-1, axis=1)
             memory["metrics"][:, -1] = new_metrics
@@ -239,12 +248,12 @@ class FiniteStateMachine:
 
     def forward_backward(
         self,
-        observed_sequence,
-        metric_function,
-        input_priors=None,
-        initial_state_distribution=None,
-        final_state_distribution=None,
-    ):
+        observed_sequence: Sequence[int],
+        metric_function: MetricFunction,
+        input_priors: Optional[npt.ArrayLike] = None,
+        initial_state_distribution: Optional[npt.ArrayLike] = None,
+        final_state_distribution: Optional[npt.ArrayLike] = None,
+    ) -> npt.NDArray[np.float64]:
         r"""
         Applies the forward-backward algorithm on a given observed sequence. The forward-backward algorithm computes the posterior pmf of each input $x_0, x_1, \ldots, x_{L-1} \in \mathcal{X}$ given an observed sequence $\mathbf{z} = (z_0, z_1, \ldots, z_{L-1}) \in \mathcal{Z}^L$. The prior pmf of each input may also be provided. See <cite>LC04, 12.6</cite>.
 
@@ -264,8 +273,8 @@ class FiniteStateMachine:
         """
         L, num_states, num_input_symbols = (
             len(observed_sequence),
-            self._num_states,
-            self._num_input_symbols,
+            self.num_states,
+            self.num_input_symbols,
         )
 
         if input_priors is None:
@@ -286,7 +295,7 @@ class FiniteStateMachine:
 
         for t, z in enumerate(observed_sequence):
             for x, s0 in np.ndindex(num_input_symbols, num_states):
-                y, s1 = self._outputs[s0, x], self._next_states[s0, x]
+                y, s1 = self.outputs[s0, x], self.next_states[s0, x]
                 log_gamma[t, s0, s1] = log_input_priors[t, x] + metric_function(y, z)
 
         for t in range(0, L - 1):
@@ -306,7 +315,7 @@ class FiniteStateMachine:
         for t in range(L):
             for x in range(num_input_symbols):
                 for s0 in range(num_states):
-                    s1 = self._next_states[s0, x]
+                    s1 = self.next_states[s0, x]
                     edge_labels[s0] = (
                         log_alpha[t, s0] + log_gamma[t, s0, s1] + log_beta[t + 1, s1]
                     )
