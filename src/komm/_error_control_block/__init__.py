0ef8eba941bdf5a19b01b7e809fb217151062046
Roberto Nobrega <rwnobrega@gmail.com>
2024-11-22 22:45:52 -0300

    Fix regression causing error in MkDocs

--- src/komm/_error_control_block/__init__.py
+++ src/komm/_error_control_block/__init__.py
@@ -30,4 +30,16 @@ __all__ = [
     "TerminatedConvolutionalCode",
 ]
 
-from .decoders import *
+from .decoders.bcjr import decode_bcjr
+from .decoders.berlekamp import decode_berlekamp
+from .decoders.exhaustive_search_hard import decode_exhaustive_search_hard
+from .decoders.exhaustive_search_soft import decode_exhaustive_search_soft
+from .decoders.majority_logic_repetition_code import (
+    decode_majority_logic_repetition_code,
+)
+from .decoders.meggitt_decoder import decode_meggitt
+from .decoders.reed import decode_reed
+from .decoders.syndrome_table import decode_syndrome_table
+from .decoders.viterbi import decode_viterbi
+from .decoders.wagner_decoder import decode_wagner
+from .decoders.weighted_reed import decode_weighted_reed
