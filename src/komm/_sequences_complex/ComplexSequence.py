7c88769625dd5b3c8682cdee6f36f171ee489025
Roberto Nobrega <rwnobrega@gmail.com>
2024-11-26 16:27:29 -0300

    Refactor `ComplexSequence`

--- src/komm/_sequences_complex/ComplexSequence.py
+++ src/komm/_sequences_complex/ComplexSequence.py
@@ -1,80 +1,75 @@
-import numpy as np
+from typing import Optional
 
+import numpy as np
+import numpy.typing as npt
+
+from .._types import ArrayIntLike
 from .._util.correlation import acorr, cyclic_acorr
 
 
 class ComplexSequence:
     r"""
     General complex sequence. It is denoted by $x[n]$, with elements in $\\mathbb{C}$. Its length (or period) is denoted by $L$.
+
+    Parameters:
+        sequence (Array1D[complex]): The complex sequence. Must be a 1D-array of length $L$ with elements in $\\mathbb{C}$.
+
+    Examples:
+        >>> seq = ComplexSequence([1, 1j, -1, -1j])
+        >>> seq.sequence
+        array([ 1.+0.j,  0.+1.j, -1.+0.j, -0.-1.j])
     """
 
-    def __init__(self, sequence):
-        r"""
-        Constructor for the class.
+    def __init__(self, sequence: npt.ArrayLike) -> None:
+        self.sequence = np.asarray(sequence, dtype=complex)
 
-        Parameters:
-            sequence (Array1D[complex]): The complex sequence. Must be a 1D-array of length $L$ with elements in $\\mathbb{C}$.
-
-        Examples:
-            >>> seq = ComplexSequence([1, 1j, -1, -1j])
-            >>> seq.sequence
-            array([ 1.+0.j,  0.+1.j, -1.+0.j, -0.-1.j])
-        """
-        self._sequence = np.array(sequence, dtype=complex)
-        self._length = self._sequence.size
-
-    def __repr__(self):
-        args = r"sequence={}".format(self._sequence.tolist())
-        return "{}({})".format(self.__class__.__name__, args)
-
-    @property
-    def sequence(self):
-        r"""
-        The complex sequence $x[n]$.
-        """
-        return self._sequence
+    def __repr__(self) -> str:
+        args = f"sequence={self.sequence.tolist()}"
+        return f"{self.__class__.__name__}({args})"
 
     @property
     def length(self):
         r"""
         The length (or period) $L$ of the complex sequence.
         """
-        return self._length
+        return self.sequence.size
 
-    def autocorrelation(self, shifts=None, normalized=False):
+    def autocorrelation(
+        self, shifts: Optional[ArrayIntLike] = None, normalized: bool = False
+    ) -> npt.NDArray[np.complex128]:
         r"""
-        Returns the autocorrelation $R[\ell]$ of the complex sequence. See [`komm.autocorrelation`](/ref/autocorrelation) for more details.
+        Returns the autocorrelation $R[\ell]$ of the complex sequence. See [`komm.acorr`](/ref/acorr) for more details.
 
         Parameters:
-            shifts (Optional[Array1D[int]]): See [`komm.autocorrelation`](/ref/autocorrelation). The default value yields $[0 : L)$.
-
-            normalized (Optional[bool]): See [`komm.autocorrelation`](/ref/autocorrelation). The default value is `False`.
+            shifts: See the corresponding parameter in [`komm.acorr`](/ref/acorr).
+            normalized: See the corresponding parameter in [`komm.acorr`](/ref/acorr).
 
         Returns:
-            autocorrelation (Array1D[complex]): The autocorrelation $R[\ell]$ of the complex sequence.
+            The autocorrelation $R[\ell]$ of the complex sequence.
 
         Examples:
             >>> seq = ComplexSequence([1, 1j, -1, -1j])
             >>> seq.autocorrelation(shifts=[-2, -1, 0, 1, 2])
             array([-2.+0.j,  0.-3.j,  4.+0.j,  0.+3.j, -2.+0.j])
         """
-        return acorr(self._sequence, shifts, normalized)
+        return acorr(self.sequence, shifts, normalized)
 
-    def cyclic_autocorrelation(self, shifts=None, normalized=False):
+    def cyclic_autocorrelation(
+        self, shifts: Optional[ArrayIntLike] = None, normalized: bool = False
+    ) -> npt.NDArray[np.complex128]:
         r"""
-        Returns the cyclic autocorrelation $\tilde{R}[\ell]$ of the complex sequence. See [`komm.cyclic_autocorrelation`](/ref/cyclic_autocorrelation) for more details.
+        Returns the cyclic autocorrelation $\tilde{R}[\ell]$ of the complex sequence. See [`komm.cyclic_acorr`](/ref/cyclic_acorr) for more details.
 
         Parameters:
-            shifts (Optional[Array1D[int]]): See [`komm.cyclic_autocorrelation`](/ref/cyclic_autocorrelation). The default value yields $[0 : L)$.
-
-            normalized (Optional[bool]): See [`komm.cyclic_autocorrelation`](/ref/cyclic_autocorrelation). The default value is `False`.
+            shifts: See the corresponding parameter in [`komm.cyclic_acorr`](/ref/cyclic_acorr).
+            normalized: See the corresponding parameter in [`komm.cyclic_acorr`](/ref/cyclic_acorr).
 
         Returns:
-            cyclic_autocorrelation (Array1D[complex]): The cyclic autocorrelation $\tilde{R}[\ell]$ of the complex sequence.
+            The cyclic autocorrelation $\tilde{R}[\ell]$ of the complex sequence.
 
         Examples:
             >>> seq = ComplexSequence([1, 1j, -1, -1j])
             >>> seq.cyclic_autocorrelation(shifts=[-2, -1, 0, 1, 2])
             array([-4.+0.j,  0.-4.j,  4.+0.j,  0.+4.j, -4.+0.j])
         """
-        return cyclic_acorr(self._sequence, shifts, normalized)
+        return cyclic_acorr(self.sequence, shifts, normalized)
