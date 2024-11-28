cc834100feb7045a29418ac6e1577fec7b1f5ba9
Roberto Nobrega <rwnobrega@gmail.com>
2024-11-28 09:12:11 -0300

    Refactor `PSKModulation`

--- src/komm/_modulation/PSKModulation.py
+++ src/komm/_modulation/PSKModulation.py
@@ -1,5 +1,9 @@
+from typing import Literal
+
+import numpy.typing as npt
+
 from .constellations import constellation_psk
-from .labelings import labelings
+from .labelings import get_labeling
 from .Modulation import Modulation
 
 
@@ -14,56 +18,55 @@ class PSKModulation(Modulation):
     <figure markdown>
       ![8-PSK constellation.](/figures/psk_8.svg)
     </figure>
+
+    Parameters:
+        order: The order $M$ of the modulation. It must be a power of $2$.
+
+        amplitude: The amplitude $A$ of the constellation. The default value is `1.0`.
+
+        phase_offset: The phase offset $\phi$ of the constellation. The default value is `0.0`.
+
+        labeling: The binary labeling of the modulation. Can be specified either as a 2D-array of integers (see [base class](/ref/Modulation) for details), or as a string. In the latter case, the string must be either `'natural'` or `'reflected'`. The default value is `'reflected'`, corresponding to the Gray labeling.
+
+    The PSK modulation with order $M = 4$, base amplitude $A = 1$, phase offset $\phi = \pi/4$, and Gray labeling is depicted below.
+
+    <figure markdown>
+      ![4-PSK modulation with Gray labeling.](/figures/psk_4_gray.svg)
+    </figure>
+
+    Examples:
+        >>> psk = komm.PSKModulation(4, phase_offset=np.pi/4.0)
+        >>> psk.constellation  # doctest: +NORMALIZE_WHITESPACE
+        array([ 0.70710678+0.70710678j, -0.70710678+0.70710678j, -0.70710678-0.70710678j,  0.70710678-0.70710678j])
+        >>> psk.labeling
+        array([[0, 0],
+               [1, 0],
+               [1, 1],
+               [0, 1]])
+        >>> psk.modulate([0, 0, 1, 1, 0, 0, 1, 0, 1, 0])  # doctest: +NORMALIZE_WHITESPACE
+        array([ 0.70710678+0.70710678j, -0.70710678-0.70710678j,  0.70710678+0.70710678j, -0.70710678+0.70710678j, -0.70710678+0.70710678j])
     """
 
-    def __init__(self, order, amplitude=1.0, phase_offset=0.0, labeling="reflected"):
-        r"""
-        Constructor for the class.
-
-        Parameters:
-            order (int): The order $M$ of the modulation. It must be a power of $2$.
-
-            amplitude (Optional[float]): The amplitude $A$ of the constellation. The default value is `1.0`.
-
-            phase_offset (Optional[float]): The phase offset $\phi$ of the constellation. The default value is `0.0`.
-
-            labeling (Optional[Array1D[int] | str]): The binary labeling of the modulation. Can be specified either as a 2D-array of integers (see [base class](/ref/Modulation) for details), or as a string. In the latter case, the string must be either `'natural'` or `'reflected'`. The default value is `'reflected'`, corresponding to the Gray labeling.
-
-        The PSK modulation with order $M = 4$, base amplitude $A = 1$, phase offset $\phi = \pi/4$, and Gray labeling is depicted below.
-
-        <figure markdown>
-          ![4-PSK modulation with Gray labeling.](/figures/psk_4_gray.svg)
-        </figure>
-
-        Examples:
-            >>> psk = komm.PSKModulation(4, phase_offset=np.pi/4.0)
-            >>> psk.constellation  # doctest: +NORMALIZE_WHITESPACE
-            array([ 0.70710678+0.70710678j, -0.70710678+0.70710678j, -0.70710678-0.70710678j,  0.70710678-0.70710678j])
-            >>> psk.labeling
-            array([[0, 0],
-                   [1, 0],
-                   [1, 1],
-                   [0, 1]])
-            >>> psk.modulate([0, 0, 1, 1, 0, 0, 1, 0, 1, 0])  # doctest: +NORMALIZE_WHITESPACE
-            array([ 0.70710678+0.70710678j, -0.70710678-0.70710678j,  0.70710678+0.70710678j, -0.70710678+0.70710678j, -0.70710678+0.70710678j])
-        """
-        self._amplitude = float(amplitude)
-        self._phase_offset = float(phase_offset)
-
-        allowed_labelings = ["natural", "reflected"]
-        if labeling in allowed_labelings:
-            labeling = labelings[labeling](order)
-        elif isinstance(labeling, str):
-            raise ValueError(
-                f"only {allowed_labelings} or 2D-arrays are allowed for the labeling"
-            )
-
+    def __init__(
+        self,
+        order: int,
+        amplitude: float = 1.0,
+        phase_offset: float = 0.0,
+        labeling: Literal["natural", "reflected"] | npt.ArrayLike = "reflected",
+    ) -> None:
         super().__init__(
-            constellation_psk(order, self._amplitude, self._phase_offset), labeling
+            constellation=constellation_psk(order, amplitude, phase_offset),
+            labeling=get_labeling(labeling, ("natural", "reflected"), order),
         )
+        self.amplitude = amplitude
+        self.phase_offset = phase_offset
+        self.labeling_parameter = labeling
 
-    def __repr__(self):
-        args = "{}, amplitude={}, phase_offset={}".format(
-            self._order, self._amplitude, self._phase_offset
-        )
-        return "{}({})".format(self.__class__.__name__, args)
+    def __repr__(self) -> str:
+        args = ", ".join([
+            f"order={self.order}",
+            f"amplitude={self.amplitude}",
+            f"phase_offset={self.phase_offset}",
+            f"labeling='{self.labeling_parameter}'",
+        ])
+        return f"PSKModulation({args})"
