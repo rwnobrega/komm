6d8aedc8d90e53fd618a58d6cd79bff8700c5b7f
Roberto Nobrega <rwnobrega@gmail.com>
2024-11-28 09:11:59 -0300

    Refactor `ASKModulation`

--- src/komm/_modulation/ASKModulation.py
+++ src/komm/_modulation/ASKModulation.py
@@ -1,5 +1,9 @@
+from typing import Literal
+
+import numpy.typing as npt
+
 from .constellations import constellation_ask
-from .labelings import labelings
+from .labelings import get_labeling
 from .Modulation import Modulation
 
 
@@ -14,69 +18,55 @@ class ASKModulation(Modulation):
     <figure markdown>
       ![4-ASK constellation.](/figures/ask_4.svg)
     </figure>
+
+    Parameters:
+        order: The order $M$ of the modulation. It must be a power of $2$.
+
+        base_amplitude: The base amplitude $A$ of the constellation. The default value is `1.0`.
+
+        phase_offset: The phase offset $\phi$ of the constellation. The default value is `0.0`.
+
+        labeling: The binary labeling of the modulation. Can be specified either as a 2D-array of integers (see [base class](/ref/Modulation) for details), or as a string. In the latter case, the string must be either `'natural'` or `'reflected'`. The default value is `'reflected'`, corresponding to the Gray labeling.
+
+    Examples:
+        The ASK modulation with order $M = 4$, base amplitude $A = 1$, and Gray labeling is depicted below.
+
+        <figure markdown>
+          ![4-ASK modulation with Gray labeling.](/figures/ask_4_gray.svg)
+        </figure>
+
+        >>> ask = komm.ASKModulation(4)
+        >>> ask.constellation
+        array([0.+0.j, 1.+0.j, 2.+0.j, 3.+0.j])
+        >>> ask.labeling
+        array([[0, 0],
+               [1, 0],
+               [1, 1],
+               [0, 1]])
+        >>> ask.modulate([0, 0, 1, 1, 0, 0, 1, 0, 1, 0])
+        array([0.+0.j, 2.+0.j, 0.+0.j, 1.+0.j, 1.+0.j])
     """
 
     def __init__(
-        self, order, base_amplitude=1.0, phase_offset=0.0, labeling="reflected"
-    ):
-        r"""
-        Constructor for the class.
-
-        Parameters:
-            order (int): The order $M$ of the modulation. It must be a power of $2$.
-
-            base_amplitude (Optional[float]): The base amplitude $A$ of the constellation. The default value is `1.0`.
-
-            phase_offset (Optional[float]): The phase offset $\phi$ of the constellation. The default value is `0.0`.
-
-            labeling (Optional[Array1D[int] | str]): The binary labeling of the modulation. Can be specified either as a 2D-array of integers (see [base class](/ref/Modulation) for details), or as a string. In the latter case, the string must be either `'natural'` or `'reflected'`. The default value is `'reflected'`, corresponding to the Gray labeling.
-
-        Examples:
-            The ASK modulation with order $M = 4$, base amplitude $A = 1$, and Gray labeling is depicted below.
-
-            <figure markdown>
-              ![4-ASK modulation with Gray labeling.](/figures/ask_4_gray.svg)
-            </figure>
-
-            >>> ask = komm.ASKModulation(4)
-            >>> ask.constellation
-            array([0.+0.j, 1.+0.j, 2.+0.j, 3.+0.j])
-            >>> ask.labeling
-            array([[0, 0],
-                   [1, 0],
-                   [1, 1],
-                   [0, 1]])
-            >>> ask.modulate([0, 0, 1, 1, 0, 0, 1, 0, 1, 0])
-            array([0.+0.j, 2.+0.j, 0.+0.j, 1.+0.j, 1.+0.j])
-        """
-        self._constructor_kwargs = {
-            "order": order,
-            "base_amplitude": base_amplitude,
-            "phase_offset": phase_offset,
-            "labeling": labeling,
-        }
-
-        self._base_amplitude = float(base_amplitude)
-        self._phase_offset = float(phase_offset)
-
-        allowed_labelings = ["natural", "reflected"]
-        if labeling in allowed_labelings:
-            labeling = labelings[labeling](order)
-        elif isinstance(labeling, str):
-            raise ValueError(
-                f"only {allowed_labelings} or 2D-arrays are allowed for the labeling"
-            )
-
+        self,
+        order: int,
+        base_amplitude: float = 1.0,
+        phase_offset: float = 0.0,
+        labeling: Literal["natural", "reflected"] | npt.ArrayLike = "reflected",
+    ) -> None:
         super().__init__(
-            constellation_ask(order, self._base_amplitude, self._phase_offset), labeling
+            constellation_ask(order, base_amplitude, phase_offset),
+            labeling=get_labeling(labeling, ("natural", "reflected"), order),
         )
+        self.base_amplitude = base_amplitude
+        self.phase_offset = phase_offset
+        self.labeling_parameter = labeling
 
-    def __repr__(self):
-        order, base_amplitude, phase_offset, labeling = (
-            self._constructor_kwargs.values()
-        )
-        args = (
-            f"{order}, base_amplitude={base_amplitude}, phase_offset={phase_offset},"
-            f" labeling={labeling}"
-        )
+    def __repr__(self) -> str:
+        args = ", ".join([
+            f"order={self.order}",
+            f"base_amplitude={self.base_amplitude}",
+            f"phase_offset={self.phase_offset}",
+            f"labeling='{self.labeling_parameter}'",
+        ])
         return f"{self.__class__.__name__}({args})"
