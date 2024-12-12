3e3954c6537d319a1fd056ffa63bae4cf0dd2bca
Roberto Nobrega <rwnobrega@gmail.com>
2024-12-12 09:25:03 -0300

    fix: update benchmarks to reflect new API

--- benchmarks/error_control_block/bench_golay_code.py
+++ benchmarks/error_control_block/bench_golay_code.py
@@ -5,17 +5,17 @@ import komm
 
 def bench_decode_golay_matrix_exhaustive_search_hard(benchmark):
     code = komm.GolayCode()
-    decoder = komm.BlockDecoder(code, method="exhaustive-search-hard")
+    decoder = komm.ExhaustiveSearchDecoder(code, input_type="hard")
     n_blocks = 1000
-    r = np.random.randint(0, 2, size=n_blocks * code.length)
+    r = np.random.randint(0, 2, size=(n_blocks, code.length))
     benchmark(decoder, r)
 
 
 def bench_decode_golay_matrix_syndrome_table(benchmark):
     code = komm.GolayCode()
-    decoder = komm.BlockDecoder(code, method="syndrome-table")
+    decoder = komm.SyndromeTableDecoder(code)
     n_blocks = 1000
-    r = np.random.randint(0, 2, size=n_blocks * code.length)
+    r = np.random.randint(0, 2, size=(n_blocks, code.length))
     benchmark(decoder, r)
 
 
@@ -23,9 +23,9 @@ def bench_decode_golay_polynomial_exhaustive_search_hard(benchmark):
     code = komm.CyclicCode(
         length=23, generator_polynomial=0b101011100011, systematic=False
     )
-    decoder = komm.BlockDecoder(code, method="exhaustive-search-hard")
+    decoder = komm.ExhaustiveSearchDecoder(code, input_type="hard")
     n_blocks = 1000
-    r = np.random.randint(0, 2, size=n_blocks * code.length)
+    r = np.random.randint(0, 2, size=(n_blocks, code.length))
     benchmark(decoder, r)
 
 
@@ -33,17 +33,17 @@ def bench_decode_golay_polynomial_syndrome_table(benchmark):
     code = komm.CyclicCode(
         length=23, generator_polynomial=0b101011100011, systematic=False
     )
-    decoder = komm.BlockDecoder(code, method="syndrome-table")
+    decoder = komm.SyndromeTableDecoder(code)
     n_blocks = 1000
-    r = np.random.randint(0, 2, size=n_blocks * code.length)
+    r = np.random.randint(0, 2, size=(n_blocks, code.length))
     benchmark(decoder, r)
 
 
-def bench_decode_golay_polynomial_meggitt_table(benchmark):
-    code = komm.CyclicCode(
-        length=23, generator_polynomial=0b101011100011, systematic=True
-    )
-    decoder = komm.BlockDecoder(code, method="meggitt")
-    n_blocks = 1000
-    r = np.random.randint(0, 2, size=n_blocks * code.length)
-    benchmark(decoder, r)
+# def bench_decode_golay_polynomial_meggitt_table(benchmark):
+#     code = komm.CyclicCode(
+#         length=23, generator_polynomial=0b101011100011, systematic=True
+#     )
+#     decoder = komm.MeggittDecoder(code)
+#     n_blocks = 1000
+#     r = np.random.randint(0, 2, size=(n_blocks, code.length)))
+#     benchmark(decoder, r)
--- benchmarks/error_control_block/bench_hamming_code.py
+++ benchmarks/error_control_block/bench_hamming_code.py
@@ -5,15 +5,15 @@ import komm
 
 def bench_decode_hamming_3_exhaustive_search_hard(benchmark):
     code = komm.HammingCode(3)
-    decoder = komm.BlockDecoder(code, method="exhaustive-search-hard")
+    decoder = komm.ExhaustiveSearchDecoder(code, input_type="hard")
     n_blocks = 1000
-    r = np.random.randint(0, 2, size=n_blocks * code.length)
+    r = np.random.randint(0, 2, size=(n_blocks, code.length))
     benchmark(decoder, r)
 
 
 def bench_decode_hamming_3_syndrome_table(benchmark):
     code = komm.HammingCode(3)
-    decoder = komm.BlockDecoder(code, method="syndrome-table")
+    decoder = komm.SyndromeTableDecoder(code)
     n_blocks = 1000
-    r = np.random.randint(0, 2, size=n_blocks * code.length)
+    r = np.random.randint(0, 2, size=(n_blocks, code.length))
     benchmark(decoder, r)
