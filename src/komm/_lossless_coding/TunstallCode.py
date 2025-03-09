9f04328b215c53040527af16c26881166b8e9059
Roberto Nobrega <rwnobrega@gmail.com>
2025-03-09 20:00:45 -0300

    refactor: convert `TunstallCode` to class

--- src/komm/_lossless_coding/TunstallCode.py
+++ src/komm/_lossless_coding/TunstallCode.py
@@ -1,20 +1,21 @@
 from dataclasses import dataclass
+from functools import cache
 from heapq import heapify, heappop, heappush
+from itertools import product
 from math import ceil, log2
 
 import numpy.typing as npt
 from tqdm import tqdm
 from typing_extensions import Self
 
+from .._util.docs import mkdocstrings
 from .._util.information_theory import PMF
 from .util import Word
 from .VariableToFixedCode import VariableToFixedCode
 
 
-def TunstallCode(
-    pmf: npt.ArrayLike,
-    target_block_size: int | None = None,
-) -> VariableToFixedCode:
+@mkdocstrings(filters=["!.*"])
+class TunstallCode(VariableToFixedCode):
     r"""
     Binary Tunstall code. It is an optimal (minimal expected rate) [variable-to-fixed length code](/ref/VariableToFixedCode) for a given probability mass function. For more details, see <cite>Say06, Sec. 3.7</cite>.
 
@@ -48,18 +49,45 @@ def TunstallCode(
         >>> code.rate(pmf)  # doctest: +FLOAT_CMP
         np.float64(1.3698630137)
     """
-    pmf = PMF(pmf)
-    if target_block_size is None:
-        target_block_size = ceil(log2(pmf.size))
-    elif 2**target_block_size < pmf.size:
-        raise ValueError("'target_block_size' is too low")
-    return VariableToFixedCode.from_sourcewords(
-        target_cardinality=2,
-        sourcewords=tunstall_algorithm(pmf, target_block_size),
-    )
+
+    def __init__(
+        self,
+        pmf: npt.ArrayLike,
+        target_block_size: int | None = None,
+    ) -> None:
+        self.pmf = PMF(pmf)
+        if target_block_size is None:
+            target_block_size = ceil(log2(self.pmf.size))
+        if 2**target_block_size < self.pmf.size:
+            raise ValueError("'target_block_size' is too low")
+        super().__init__(
+            target_cardinality=2,
+            source_cardinality=self.pmf.size,
+            target_block_size=target_block_size,
+            dec_mapping=tunstall_algorithm(self.pmf, target_block_size),
+        )
+
+    def __repr__(self) -> str:
+        args = ", ".join([
+            f"pmf={self.pmf.tolist()}",
+            f"target_block_size={self.target_block_size}",
+        ])
+        return f"{self.__class__.__name__}({args})"
+
+    @cache
+    def is_fully_covering(self) -> bool:
+        return True
+
+    @cache
+    def is_uniquely_encodable(self) -> bool:
+        return True
+
+    @cache
+    def is_prefix_free(self) -> bool:
+        return True
 
 
-def tunstall_algorithm(pmf: PMF, code_block_size: int) -> list[Word]:
+def tunstall_algorithm(pmf: PMF, code_block_size: int) -> dict[Word, Word]:
     @dataclass
     class Node:
         sourceword: Word
@@ -85,4 +113,10 @@ def tunstall_algorithm(pmf: PMF, code_block_size: int) -> list[Word]:
 
     pbar.close()
 
-    return sorted(node.sourceword for node in heap)
+    dec_mapping = dict(
+        zip(
+            product([0, 1], repeat=code_block_size),
+            sorted(node.sourceword for node in heap),
+        )
+    )
+    return dec_mapping
