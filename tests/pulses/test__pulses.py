1d9babfa6ac92f040af74ddc80a938469041439c
Roberto Nobrega <rwnobrega@gmail.com>
2025-05-03 11:23:31 -0300

    test: add tests for autocorrelation of pulses

---
+++ tests/pulses/test__pulses.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pytest
+
+import komm
+import komm.abc
+
+PULSES = [
+    komm.RectangularPulse(),
+    komm.ManchesterPulse(),
+    # komm.SincPulse(),
+    komm.RaisedCosinePulse(0.3),
+    komm.RaisedCosinePulse(0.75),
+    komm.RaisedCosinePulse(),
+    komm.RootRaisedCosinePulse(0.25),
+    komm.RootRaisedCosinePulse(0.5),
+    komm.RootRaisedCosinePulse(),
+    komm.GaussianPulse(0.3),
+    komm.GaussianPulse(0.75),
+    komm.GaussianPulse(),
+]
+
+
+@pytest.mark.parametrize("pulse", PULSES)
+def test_pulses_energy_density_spectrum_vs_autocorrelation(pulse: komm.abc.Pulse):
+    τs = np.linspace(-20.0, 20.0, 10001)
+    fs = np.linspace(-2.0, 2.0, 101)
+    R = pulse.autocorrelation(τs)
+    S = np.array([np.trapezoid(R * np.cos(-2 * np.pi * f * τs), τs) for f in fs])
+    np.testing.assert_allclose(S, pulse.energy_density_spectrum(fs), atol=1e-3)
+
+
+@pytest.mark.parametrize("pulse", PULSES)
+def test_pulses_waveform_vs_autocorrelation(pulse: komm.abc.Pulse):
+    ts = np.linspace(-20.0, 20.0, 10001)
+    τs = np.linspace(-2.0, 2.0, 101)
+    h = pulse.waveform(ts)
+    R = np.array([np.trapezoid(h * np.interp(ts + τ, ts, h), ts) for τ in τs])
+    np.testing.assert_allclose(R, pulse.autocorrelation(τs), atol=1e-3)
