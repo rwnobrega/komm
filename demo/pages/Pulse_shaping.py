6c7eff9f9446b3ea5bf67cf654ab8d7820462d63
Roberto Nobrega <rwnobrega@gmail.com>
2025-05-09 14:45:30 -0300

    refactor: update pulse shaping demo

--- demo/pages/Pulse_shaping.py
+++ demo/pages/Pulse_shaping.py
@@ -12,20 +12,20 @@ st.header("Raised cosine pulse")
 
 cols = st.columns(3)
 with cols[0]:
+    sps = st.slider(
+        "Samples per symbol",
+        min_value=2,
+        max_value=32,
+        value=16,
+        step=2,
+    )
+with cols[1]:
     rolloff = st.slider(
         "Roll-off factor",
         min_value=0.0,
         max_value=1.0,
         value=0.25,
         step=0.01,
-    )
-with cols[1]:
-    samples_per_symbol = st.slider(
-        "Samples per symbol",
-        min_value=2,
-        max_value=32,
-        value=16,
-        step=2,
     )
 with cols[2]:
     truncation = st.slider(
@@ -36,32 +36,30 @@ with cols[2]:
         step=2,
     )
 
-seq = [1, -1, 1, 1, -1, -1, 1]
+dms = komm.DiscreteMemorylessSource(
+    np.ones(4) / 4,
+    rng=np.random.default_rng(seed=42),
+)
+
+bits = dms(10)
+seq = 2 * bits - 3
 
 pulse = komm.RaisedCosinePulse(rolloff=rolloff)
 tx_filter = komm.TransmitFilter(
     pulse,
-    samples_per_symbol=samples_per_symbol,
+    samples_per_symbol=sps,
     truncation=truncation,
 )
-t = tx_filter.time(seq)
-waveform = tx_filter(seq)
-spectrum = fft.fftshift(fft.fft(waveform, 1024)) / samples_per_symbol
+t, _ = tx_filter.axes(seq)
+yt = tx_filter(seq)
 
-fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(12, 4))
-ax0.stem(seq, linefmt="r", markerfmt="ro")
-ax0.plot(t, waveform, "o-", markersize=2)
-ax0.axis((-3, 9, -2, 2))
-ax0.set_xlabel("$t$")
-ax0.set_ylabel("$y(t)$")
-ax0.grid()
+fig, ax = plt.subplots(figsize=(12, 4))
+ax.stem(seq, linefmt="r", markerfmt="ro")
+ax.plot(t, yt, "o-", markersize=2)
+ax.axis((-3, 12, -4, 4))
+ax.set_xlabel("$t / T_b$")
+ax.set_ylabel("$y(t)$")
+ax.grid()
 
-f = np.linspace(-0.5, 0.5, 1024, endpoint=False)
-ax1.plot(f, np.abs(spectrum))
-ax1.axis((-0.5, 0.5, -0.5, 4.5))
-ax1.set_xlabel("$f$")
-ax1.set_ylabel("$|\\hat{y}(f)|$")
-ax1.grid()
-
-
+fig.tight_layout()
 st.pyplot(fig)
