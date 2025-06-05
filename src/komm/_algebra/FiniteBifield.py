e553d73cefb80fab7acc8e5d4f5b1f33b1b1a263
Roberto Nobrega <rwnobrega@gmail.com>
2025-06-05 13:36:28 -0300

    chore: return `NotImplemented` for consistency

--- src/komm/_algebra/FiniteBifield.py
+++ src/komm/_algebra/FiniteBifield.py
@@ -184,7 +184,7 @@ class FiniteBifield:
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, self.__class__):
-            return False
+            return NotImplemented
         return self.degree == other.degree and self.modulus == other.modulus
 
     @property
