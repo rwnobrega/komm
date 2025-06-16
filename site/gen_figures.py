8e7a9a008c010600e77334a78924c07b00cae775
Roberto Nobrega <rwnobrega@gmail.com>
2025-06-15 21:36:27 -0300

    docs: make doc figures dark-mode-friendly

--- site/gen_figures.py
+++ site/gen_figures.py
@@ -1,4 +1,50 @@
 import os
+import xml.etree.ElementTree as ET
+
+
+def is_black_rgb(value: str) -> bool:
+    return value.strip().lower() in {
+        "black",
+        "#000",
+        "#000000",
+        "rgb(0%, 0%, 0%)",
+        "rgb(0,0,0)",
+        "rgb(0, 0, 0)",
+    }
+
+
+def patch_svg(path):
+    ET.register_namespace("", "http://www.w3.org/2000/svg")
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    style_element = ET.fromstring("""
+    <style>
+      .fill { fill: black; }
+      .stroke { stroke: black; }
+      @media (prefers-color-scheme: dark) {
+        .fill { fill: white; }
+        .stroke { stroke: white; }
+      }
+    </style>
+    """)
+    root.insert(0, style_element)
+
+    for elem in root.iter():
+        classes = []
+        fill = elem.attrib.get("fill")
+        if fill and is_black_rgb(fill):
+            del elem.attrib["fill"]
+            classes.append("fill")
+        stroke = elem.attrib.get("stroke")
+        if stroke and is_black_rgb(stroke):
+            del elem.attrib["stroke"]
+            classes.append("stroke")
+        if classes:
+            prev_class = elem.attrib.get("class", "")
+            elem.attrib["class"] = f"{prev_class} {' '.join(classes)}".strip()
+
+    tree.write(path)
 
 
 def main():
@@ -14,6 +60,8 @@ def main():
                 continue
             os.system(f"iperender -svg {src_path} {dst_path}")
             print(f"Generated {dst_path}")
+            patch_svg(dst_path)
+
     # Now, delete svg files that don't have a corresponding pdf
     for filename in os.listdir("docs/fig"):
         if filename.endswith(".svg"):
