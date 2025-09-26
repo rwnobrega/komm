c992c196d5d65b7ecf6c7d8aeabc2a06c1f50031
Roberto Nobrega <rwnobrega@gmail.com>
2025-09-25 22:05:48 -0300

    tests: update LZ77 tests for consistency with other compression classes

--- tests/lossless_coding/test_lz77.py
+++ tests/lossless_coding/test_lz77.py
@@ -1,5 +1,5 @@
 from itertools import product
-from math import ceil
+from math import ceil, log2
 
 import numpy as np
 import pytest
@@ -7,30 +7,109 @@ import pytest
 import komm
 
 
-def test_lz77_empty_and_single():
+# Helper function for target_cardinality = 2 only
+def len_compressed(A, W, L, num_triples):
+    return (ceil(log2(W + 1)) + ceil(log2(L + 1)) + ceil(log2(A))) * num_triples
+
+
+@pytest.mark.parametrize(
+    "alphabet, message, parameters, triples",
+    [
+        # [Abrantes, p. 26]
+        (
+            "ABC",
+            "AAAABABCCAABACCAAAABC",
+            (12, 4),
+            [
+                (0, 0, 0),
+                (1, 3, 1),
+                (2, 2, 2),
+                (1, 1, 0),
+                (7, 3, 2),
+                (6, 3, 0),
+                (8, 2, 2),
+            ],
+        ),
+        # [https://blog.coderspirit.xyz/blog/2023/06/04/exploring-the-lz77-algorithm]
+        (
+            "abdkr",
+            "abadakadabra",
+            (8, 4),
+            [
+                (0, 0, 0),
+                (0, 0, 1),
+                (2, 1, 2),
+                (4, 1, 3),
+                (4, 3, 1),
+                (0, 0, 4),
+                (0, 0, 0),
+            ],
+        ),
+        # [Sayood, p. 122]
+        (
+            "abcdr",
+            "cabracadabrarrarrad",
+            (13, 6),
+            [
+                (0, 0, 2),
+                (0, 0, 0),
+                (0, 0, 1),
+                (0, 0, 4),
+                (3, 1, 2),
+                (5, 1, 3),
+                (7, 4, 4),
+                (3, 5, 3),
+            ],
+        ),
+    ],
+)
+def test_lz77_literature(alphabet, message, parameters, triples):
+    window_size, lookahead_size = parameters
     code = komm.LempelZiv77Code(
-        window_size=16,
-        lookahead_size=4,
-        source_cardinality=2,
+        window_size=window_size,
+        lookahead_size=lookahead_size,
+        source_cardinality=len(alphabet),
         target_cardinality=2,
     )
-    assert code.encode([]).tolist() == []
-    assert code.decode([]).tolist() == []
-    for symbol in [0, 1]:
-        msg = [symbol]
-        np.testing.assert_equal(code.decode(code.encode(msg)), msg)
+    A, W, L = len(alphabet), window_size, lookahead_size
+    message = [alphabet.index(char) for char in message]
+    np.testing.assert_equal(code.source_to_triples(message), triples)
+    compressed = code.encode(message)
+    assert len(compressed) == len_compressed(A, W, L, len(triples))
+    np.testing.assert_equal(code.decode(compressed), message)
 
 
+@pytest.mark.parametrize("window_size", [16, 20, 31])
+@pytest.mark.parametrize("lookahead_size", [8, 12, 17])
 @pytest.mark.parametrize("source_cardinality", [2, 4, 8])
-def test_lz77_roundtrip_random(source_cardinality):
+@pytest.mark.parametrize("target_cardinality", [2, 4, 8])
+def test_lz77_general(
+    window_size,
+    lookahead_size,
+    source_cardinality,
+    target_cardinality,
+):
     code = komm.LempelZiv77Code(
-        window_size=32,
-        lookahead_size=8,
-        source_cardinality=source_cardinality,
+        window_size, lookahead_size, source_cardinality, target_cardinality
     )
 
-    msg = np.random.randint(0, source_cardinality, size=100).tolist()
-    np.testing.assert_equal(code.decode(code.encode(msg)), msg)
+    # Check code parameters
+    assert code.window_size == window_size
+    assert code.lookahead_size == lookahead_size
+    assert code.source_cardinality == source_cardinality
+    assert code.target_cardinality == target_cardinality
+
+    # Encode and decode empty input
+    np.testing.assert_equal(code.encode([]), [])
+    np.testing.assert_equal(code.decode([]), [])
+
+    # Single-symbol message
+    for symbol in range(source_cardinality):
+        np.testing.assert_equal(code.decode(code.encode([symbol])), [symbol])
+
+    # Random message
+    message = np.random.randint(0, source_cardinality, 1000)
+    np.testing.assert_equal(code.decode(code.encode(message)), message)
 
 
 @pytest.mark.parametrize("k", range(2, 6))
@@ -53,83 +132,3 @@ def test_lz77_worst_case(k):
     compressed = code.encode(msg)
     np.testing.assert_equal(code.decode(compressed), msg)
     assert len(compressed) <= len(msg) * 10  # sanity bound
-
-
-@pytest.mark.parametrize(
-    "alphabet, message, triples, window_size, lookahead_size, len_compressed",
-    [
-        # [Abrantes, p. 26]
-        (
-            "ABC",
-            "AAAABABCCAABACCAAAABC",
-            [
-                (0, 0, 0),
-                (1, 3, 1),
-                (2, 2, 2),
-                (1, 1, 0),
-                (7, 3, 2),
-                (6, 3, 0),
-                (8, 2, 2),
-            ],
-            12,
-            4,
-            (ceil(np.log2(8 + 1)) + ceil(np.log2(4 + 1)) + ceil(np.log2(len("ABC"))))
-            * 7,
-        ),
-        # [https://blog.coderspirit.xyz/blog/2023/06/04/exploring-the-lz77-algorithm]
-        (
-            "abdkr",
-            "abadakadabra",
-            [
-                (0, 0, 0),
-                (0, 0, 1),
-                (2, 1, 2),
-                (4, 1, 3),
-                (4, 3, 1),
-                (0, 0, 4),
-                (0, 0, 0),
-            ],
-            8,
-            4,
-            (ceil(np.log2(8 + 1)) + ceil(np.log2(4 + 1)) + ceil(np.log2(len("abdkr"))))
-            * 7,
-        ),
-        # [Sayood, p. 122]
-        (
-            "abcdr",
-            "cabracadabrarrarrad",
-            [
-                (0, 0, 2),
-                (0, 0, 0),
-                (0, 0, 1),
-                (0, 0, 4),
-                (3, 1, 2),
-                (5, 1, 3),
-                (7, 4, 4),
-                (3, 5, 3),
-            ],
-            13,
-            6,
-            (ceil(np.log2(13 + 1)) + ceil(np.log2(6 + 1)) + ceil(np.log2(len("abcdr"))))
-            * 8,
-        ),
-    ],
-)
-def test_lz77_examples(
-    alphabet, message, triples, window_size, lookahead_size, len_compressed
-):
-    code = komm.LempelZiv77Code(
-        window_size=window_size,
-        lookahead_size=lookahead_size,
-        source_cardinality=len(alphabet),
-        target_cardinality=2,
-    )
-
-    msg_indices = [alphabet.index(char) for char in message]
-
-    print(code.source_to_triples(msg_indices))
-    np.testing.assert_equal(code.source_to_triples(msg_indices), triples)
-    compressed = code.encode(msg_indices)
-
-    assert len(compressed) == len_compressed
-    np.testing.assert_equal(code.decode(compressed), msg_indices)
