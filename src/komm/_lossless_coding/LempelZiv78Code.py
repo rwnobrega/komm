acb4c50b1a94eb8751d6778fd2ae3f9941d2a86f
Roberto Nobrega <rwnobrega@gmail.com>
2025-10-02 22:10:17 -0300

    chore: update LZ78 and LZW for consistency

--- src/komm/_lossless_coding/LempelZiv78Code.py
+++ src/komm/_lossless_coding/LempelZiv78Code.py
@@ -13,9 +13,12 @@ class LempelZiv78Code:
     r"""
     Lempel–Ziv 78 (LZ78 or LZ2) code. It is a lossless data compression algorithm which is asymptotically optimal for ergodic sources. For more details, see <cite>Say06, Sec. 5.4.2</cite> and <cite>CT06, Sec. 13.4.2</cite>.
 
+    Note:
+        Here, for simplicity, we assume that the source alphabet is $\mathcal{X} = [0 : |\mathcal{X}|)$ and the target alphabet is $\mathcal{Y} = [0 : |\mathcal{Y}|)$, where $|\mathcal{X}| \geq 2$ and $|\mathcal{Y}| \geq 2$ are called the *source cardinality* and *target cardinality*, respectively.
+
     Parameters:
-        source_cardinality: The source cardinality $S$. Must be an integer greater than or equal to $2$.
-        target_cardinality: The target cardinality $T$. Must be an integer greater than or equal to $2$. Default is $2$ (binary).
+        source_cardinality: The source cardinality $|\mathcal{X}|$. Must satisfy $|\mathcal{X}| \geq 2$.
+        target_cardinality: The target cardinality $|\mathcal{Y}|$. Must satisfy $|\mathcal{Y}| \geq 2$. The default value is $2$ (binary).
 
     Examples:
         >>> lz78 = komm.LempelZiv78Code(2)  # Binary source, binary target
@@ -33,13 +36,13 @@ class LempelZiv78Code:
 
     def encode(self, input: npt.ArrayLike) -> npt.NDArray[np.integer]:
         r"""
-        Encodes a sequence of source symbols using the LZ78 encoding algorithm.
+        Encodes a sequence of source symbols to a sequence of target symbols.
 
         Parameters:
-            input: The sequence of symbols to be encoded. Must be a 1D-array with elements in $[0:S)$ (where $S$ is the source cardinality of the code).
+            input: The sequence of source symbols to be encoded. Must be a 1D-array with elements in $\mathcal{X}$.
 
         Returns:
-            output: The sequence of encoded symbols. It is a 1D-array with elements in $[0:T)$ (where $T$ is the target cardinality of the code).
+            output: The sequence of encoded target symbols. It is a 1D-array with elements in $\mathcal{Y}$.
 
         Examples:
             >>> lz78 = komm.LempelZiv78Code(2)
@@ -50,8 +53,8 @@ class LempelZiv78Code:
             >>> lz78.encode(np.zeros(15, dtype=int))
             array([0, 1, 0, 2, 0, 3, 0, 4, 0])
         """
-        T = self.target_cardinality
-        M = ceil(log(self.source_cardinality, T))
+        calY = self.target_cardinality
+        M = ceil(log(self.source_cardinality, calY))
         input = np.asarray(input)
         dictionary: dict[Word, int] = {(): 0}
         output: list[int] = []
@@ -61,29 +64,29 @@ class LempelZiv78Code:
             if word + (symbol,) in dictionary:
                 word += (symbol,)
                 continue
-            k = ceil(log(len(dictionary), T))
+            k = ceil(log(len(dictionary), calY))
             pointer = dictionary[word]
-            output.extend(integer_to_symbols(pointer, base=T, width=k))
-            output.extend(integer_to_symbols(symbol, base=T, width=M))
+            output.extend(integer_to_symbols(pointer, base=calY, width=k))
+            output.extend(integer_to_symbols(symbol, base=calY, width=M))
             dictionary[word + (symbol,)] = len(dictionary)
             word = ()
 
         if word:
-            k = ceil(log(len(dictionary), T))
+            k = ceil(log(len(dictionary), calY))
             pointer = dictionary[word]
-            output.extend(integer_to_symbols(pointer, base=T, width=k))
+            output.extend(integer_to_symbols(pointer, base=calY, width=k))
 
         return np.array(output, dtype=int)
 
     def decode(self, input: npt.ArrayLike) -> npt.NDArray[np.integer]:
         r"""
-        Decodes a sequence of encoded symbols using the LZ78 decoding algorithm.
+        Decodes a sequence of target symbols to a sequence of source symbols.
 
         Parameters:
-            input: The sequence of symbols to be decoded. Must be a 1D-array with elements in $[0:T)$ (where $T$ is the target cardinality of the code). Also, the sequence must be a valid output of the `encode` method.
+            input: The sequence of target symbols to be decoded. Must be a 1D-array with elements in $\mathcal{Y}$. Also, the sequence must be a valid output of the `encode` method.
 
         Returns:
-            output: The sequence of decoded symbols. It is a 1D-array with elements in $[0:S)$ (where $S$ is the source cardinality of the code).
+            output: The sequence of decoded source symbols. It is a 1D-array with elements in $\mathcal{X}$.
 
         Examples:
             >>> lz78 = komm.LempelZiv78Code(2)
@@ -94,8 +97,8 @@ class LempelZiv78Code:
             >>> lz78.decode([0, 1, 0, 2, 0, 3, 0, 4, 0])
             array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         """
-        T = self.target_cardinality
-        M = ceil(log(self.source_cardinality, T))
+        calY = self.target_cardinality
+        M = ceil(log(self.source_cardinality, calY))
         input = np.asarray(input)
         dictionary: dict[int, Word] = {0: ()}
         output: list[int] = []
@@ -103,17 +106,17 @@ class LempelZiv78Code:
         pbar = tqdm(total=input.size, desc="Decompressing LZ78", delay=2.5)
         i = 0
         while True:
-            k = ceil(log(len(dictionary), T))
+            k = ceil(log(len(dictionary), calY))
             if i + k > input.size:
                 break
-            pointer = symbols_to_integer(input[i : i + k], base=T)
+            pointer = symbols_to_integer(input[i : i + k], base=calY)
             word = dictionary[pointer]
             output.extend(word)
             i += k
             pbar.update(k)
             if i + M > input.size:
                 break
-            symbol = symbols_to_integer(input[i : i + M], base=T)
+            symbol = symbols_to_integer(input[i : i + M], base=calY)
             output.append(symbol)
             dictionary[len(dictionary)] = word + (symbol,)
             i += M
--- src/komm/_lossless_coding/LempelZivWelchCode.py
+++ src/komm/_lossless_coding/LempelZivWelchCode.py
@@ -13,9 +13,12 @@ class LempelZivWelchCode:
     r"""
     Lempel–Ziv–Welch (LZW) code. It is a lossless data compression algorithm which is variation of the [Lempel–Ziv 78](/ref/LempelZiv78Code) algorithm. For more details, see <cite>Say06, Sec. 5.4.2</cite>.
 
+    Note:
+        Here, for simplicity, we assume that the source alphabet is $\mathcal{X} = [0 : |\mathcal{X}|)$ and the target alphabet is $\mathcal{Y} = [0 : |\mathcal{Y}|)$, where $|\mathcal{X}| \geq 2$ and $|\mathcal{Y}| \geq 2$ are called the *source cardinality* and *target cardinality*, respectively.
+
     Parameters:
-        source_cardinality: The source cardinality $S$. Must be an integer greater than or equal to $2$.
-        target_cardinality: The target cardinality $T$. Must be an integer greater than or equal to $2$. Default is $2$ (binary).
+        source_cardinality: The source cardinality $|\mathcal{X}|$. Must satisfy $|\mathcal{X}| \geq 2$.
+        target_cardinality: The target cardinality $|\mathcal{Y}|$. Must satisfy $|\mathcal{Y}| \geq 2$. The default value is $2$ (binary).
 
     Examples:
         >>> lzw = komm.LempelZivWelchCode(2)  # Binary source, binary target
@@ -33,13 +36,13 @@ class LempelZivWelchCode:
 
     def encode(self, input: npt.ArrayLike) -> npt.NDArray[np.integer]:
         r"""
-        Encodes a sequence of source symbols using the LZW encoding algorithm.
+        Encodes a sequence of source symbols to a sequence of target symbols.
 
         Parameters:
-            input: The sequence of symbols to be encoded. Must be a 1D-array with elements in $[0:S)$ (where $S$ is the source cardinality of the code).
+            input: The sequence of source symbols to be encoded. Must be a 1D-array with elements in $\mathcal{X}$.
 
         Returns:
-            output: The sequence of encoded symbols. It is a 1D-array with elements in $[0:T)$ (where $T$ is the target cardinality of the code).
+            output: The sequence of encoded target symbols. It is a 1D-array with elements in $\mathcal{Y}$.
 
         Examples:
             >>> lzw = komm.LempelZivWelchCode(2)
@@ -50,10 +53,9 @@ class LempelZivWelchCode:
             >>> lzw.encode(np.zeros(15, dtype=int))
             array([0, 2, 3, 4, 5])
         """
-        T = self.target_cardinality
-        S = self.source_cardinality
+        calX, calY = self.source_cardinality, self.target_cardinality
         input = np.asarray(input)
-        dictionary: dict[Word, int] = {(s,): s for s in range(S)}
+        dictionary: dict[Word, int] = {(s,): s for s in range(calX)}
         output: list[int] = []
 
         word: Word = ()
@@ -61,28 +63,28 @@ class LempelZivWelchCode:
             if word + (symbol,) in dictionary:
                 word += (symbol,)
                 continue
-            k = ceil(log(len(dictionary), T))
+            k = ceil(log(len(dictionary), calY))
             pointer = dictionary[word]
-            output.extend(integer_to_symbols(pointer, base=T, width=k))
+            output.extend(integer_to_symbols(pointer, base=calY, width=k))
             dictionary[word + (symbol,)] = len(dictionary)
             word = (symbol,)
 
         if word:
-            k = ceil(log(len(dictionary), T))
+            k = ceil(log(len(dictionary), calY))
             pointer = dictionary[word]
-            output.extend(integer_to_symbols(pointer, base=T, width=k))
+            output.extend(integer_to_symbols(pointer, base=calY, width=k))
 
         return np.array(output, dtype=int)
 
     def decode(self, input: npt.ArrayLike) -> npt.NDArray[np.integer]:
         r"""
-        Decodes a sequence of encoded symbols using the LZW decoding algorithm.
+        Decodes a sequence of target symbols to a sequence of source symbols.
 
         Parameters:
-            input: The sequence of symbols to be decoded. Must be a 1D-array with elements in $[0:T)$ (where $T$ is the target cardinality of the code). Also, the sequence must be a valid output of the `encode` method.
+            input: The sequence of target symbols to be decoded. Must be a 1D-array with elements in $\mathcal{Y}$. Also, the sequence must be a valid output of the `encode` method.
 
         Returns:
-            output: The sequence of decoded symbols. It is a 1D-array with elements in $[0:S)$ (where $S$ is the source cardinality of the code).
+            output: The sequence of decoded source symbols. It is a 1D-array with elements in $\mathcal{X}$.
 
         Examples:
             >>> lzw = komm.LempelZivWelchCode(2)
@@ -93,28 +95,27 @@ class LempelZivWelchCode:
             >>> lzw.decode([0, 2, 3, 4, 5])
             array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         """
-        T = self.target_cardinality
-        S = self.source_cardinality
+        calX, calY = self.source_cardinality, self.target_cardinality
         input = np.asarray(input)
 
         if input.size == 0:
             return np.array([], dtype=int)
 
-        dictionary: dict[int, Word] = {s: (s,) for s in range(S)}
+        dictionary: dict[int, Word] = {s: (s,) for s in range(calX)}
         output: list[int] = []
 
-        k = ceil(log(S, T))
-        pointer = symbols_to_integer(input[:k], base=T)
+        k = ceil(log(calX, calY))
+        pointer = symbols_to_integer(input[:k], base=calY)
         old = dictionary[pointer]
         output.extend(old)
 
         i = k
         pbar = tqdm(total=input.size, desc="Decompressing LZW", delay=2.5, initial=k)
         while True:
-            k = ceil(log(len(dictionary) + 1, T))
+            k = ceil(log(len(dictionary) + 1, calY))
             if i + k > input.size:
                 break
-            pointer = symbols_to_integer(input[i : i + k], base=T)
+            pointer = symbols_to_integer(input[i : i + k], base=calY)
             word = dictionary.get(pointer, old + (old[0],))
             output.extend(word)
             dictionary[len(dictionary)] = old + (word[0],)
