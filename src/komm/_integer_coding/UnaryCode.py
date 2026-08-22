99aec67b71e917c4892076f755e1321b693a65da
Roberto Nobrega <rwnobrega@gmail.com>
2026-08-21 23:58:04 -0300

    chore!: change unary code convention to agree with MacK03.

--- src/komm/_integer_coding/UnaryCode.py
+++ src/komm/_integer_coding/UnaryCode.py
@@ -7,19 +7,19 @@ from .._util.validators import validate_integer_min, validate_integer_range
 
 class UnaryCode(abc.IntegerCode):
     r"""
-    Unary code. It is an integer code. For the definition of this code, see [Wikipedia: Unary coding](https://en.wikipedia.org/wiki/Unary_coding).
+    Unary code. It is an integer code. The codeword for a non-negative integer $n$ consists of $n - 1$ zeros followed by a single $1$. For more details, see <cite>MacK03, Ch. 7</cite>.
     """
 
     def encode(self, input: npt.ArrayLike) -> npt.NDArray[np.integer]:
         r"""
-        For the unary code, the integers must be non-negative.
+        For the unary code, the integers must be positive.
 
         Examples:
             >>> code = komm.UnaryCode()
             >>> code.encode([4, 1, 3])
-            array([1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0])
+            array([0, 0, 0, 1, 1, 0, 0, 1])
         """
-        input = validate_integer_min(input, low=0)
+        input = validate_integer_min(input, low=1)
         if input.size == 0:
             return np.array([], dtype=int)
         return np.concatenate([unary_encode(i) for i in input])
@@ -28,7 +28,7 @@ class UnaryCode(abc.IntegerCode):
         r"""
         Examples:
             >>> code = komm.UnaryCode()
-            >>> code.decode([1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0])
+            >>> code.decode([0, 0, 0, 1, 1, 0, 0, 1])
             array([4, 1, 3])
         """
         input = validate_integer_range(input, low=0, high=2)
@@ -36,7 +36,7 @@ class UnaryCode(abc.IntegerCode):
         i = 0
         while i < input.size:
             j = i
-            while j < input.size and input[j] == 1:
+            while j < input.size and input[j] == 0:
                 j += 1
             if j == input.size:
                 raise ValueError("input contains an incomplete codeword")
@@ -46,8 +46,8 @@ class UnaryCode(abc.IntegerCode):
 
 
 def unary_encode(integer: int) -> list[int]:
-    return [1] * integer + [0]
+    return [0] * (integer - 1) + [1]
 
 
 def unary_decode(bits: list[int]) -> int:
-    return len(bits) - 1
+    return len(bits)
--- tests/integer_coding/test_unary_code.py
+++ tests/integer_coding/test_unary_code.py
@@ -5,33 +5,28 @@ import komm
 
 
 def test_unary_basic():
-    message = [0, 1, 2, 3, 4]
-    encoded = [0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0]
+    message = [1, 2, 3, 4]
+    encoded = [1, 0, 1, 0, 0, 1, 0, 0, 0, 1]
     code = komm.UnaryCode()
     np.testing.assert_equal(code.encode(message), encoded)
     np.testing.assert_equal(code.decode(encoded), message)
 
 
-@pytest.mark.parametrize("n", range(200))
+@pytest.mark.parametrize("n", range(1, 200))
 def test_unary_code_length(n):
     code = komm.UnaryCode()
     encoded = code.encode([n])
-    assert len(encoded) == n + 1
+    assert len(encoded) == n
 
 
 def test_unary_roundtrip():
     code = komm.UnaryCode()
-    message = list(range(0, 50))  # Unary domain: non-negative integers.
+    message = list(range(1, 50))  # Unary domain: positive integers.
     np.testing.assert_equal(code.decode(code.encode(message)), message)
 
 
-def test_unary_encode_accepts_zero():
-    code = komm.UnaryCode()
-    np.testing.assert_equal(code.encode([0]), [0])
-
-
-@pytest.mark.parametrize("message", [[-1], [-3], [0, 1, -2]])
-def test_unary_encode_rejects_negative(message):
+@pytest.mark.parametrize("message", [[0], [-1], [1, 0, 2]])
+def test_unary_encode_rejects_non_positive(message):
     code = komm.UnaryCode()
     with pytest.raises(ValueError, match="invalid entries"):
         code.encode(message)
@@ -44,7 +39,7 @@ def test_decode_rejects_non_binary_bits(stream):
         code.decode(stream)
 
 
-@pytest.mark.parametrize("stream", [[1, 1, 1]])
+@pytest.mark.parametrize("stream", [[0, 0, 0]])
 def test_unary_decode_rejects_incomplete_codeword(stream):
     code = komm.UnaryCode()
     with pytest.raises(ValueError, match="incomplete codeword"):
