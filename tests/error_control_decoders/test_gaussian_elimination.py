6b4d152e02076fd77c121370674ddfa34ecd3979
Roberto Nobrega <rwnobrega@gmail.com>
2026-09-14 16:06:28 -0300

    test: add exhaustive bitwise MAP oracle for `GaussianEliminationDecoder`

--- tests/error_control_decoders/test_gaussian_elimination.py
+++ tests/error_control_decoders/test_gaussian_elimination.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import numpy as np
 import pytest
 
@@ -8,6 +10,7 @@ import komm.abc
 @pytest.fixture(
     params=[
         komm.HammingCode(3),
+        komm.BlockCode([[1, 0, 0, 1, 1], [0, 1, 1, 1, 0]]),
         komm.RepetitionCode(5),
         komm.SingleParityCheckCode(5),
         komm.CordaroWagnerCode(5),
@@ -16,6 +19,28 @@ import komm.abc
 )
 def code(request: pytest.FixtureRequest) -> komm.abc.BlockCode:
     return request.param
+
+
+def exhaustive_bitwise_map_decoder(code: komm.abc.BlockCode, b):
+    codewords = code.codewords()
+    compat_v = codewords[np.all((b == 2) | (b == codewords), axis=1)]
+    compat_u = code.inverse_encode(compat_v)
+    v_hat = np.where(np.all(compat_v == compat_v[0], axis=0), compat_v[0], 2)
+    u_hat = np.where(np.all(compat_u == compat_u[0], axis=0), compat_u[0], 2)
+    return v_hat, u_hat
+
+
+@pytest.mark.parametrize(
+    "code", [komm.HammingCode(3), komm.BlockCode([[1, 0, 0, 1, 1], [0, 1, 1, 1, 0]])]
+)
+def test_gaussian_elimination_exhaustive(code: komm.abc.BlockCode):
+    decoder = komm.GaussianEliminationDecoder(code)
+    for u in product([0, 1], repeat=code.dimension):
+        v = code.encode(u)
+        for mask in product([False, True], repeat=code.length):
+            b = np.where(mask, 2, v)
+            v_hat, u_hat = exhaustive_bitwise_map_decoder(code, b)
+            np.testing.assert_array_equal(decoder.decode(b), u_hat)
 
 
 def test_gaussian_elimination_correct_bits(code: komm.abc.BlockCode):
