de7d4ff37470b71707cb4ed888f934f45210943b
Roberto Nobrega <rwnobrega@gmail.com>
2026-09-17 23:26:50 -0300

    test: check that both decoding paths agree

--- tests/error_control_decoders/test_gaussian_elimination.py
+++ tests/error_control_decoders/test_gaussian_elimination.py
@@ -5,22 +5,29 @@ import pytest
 
 import komm
 import komm.abc
-
-
-@pytest.fixture(
-    params=[
-        komm.HammingCode(3),
-        komm.BlockCode([[1, 0, 0, 1, 1], [0, 1, 1, 1, 0]]),
-        komm.RepetitionCode(5),
-        komm.SingleParityCheckCode(5),
-        komm.CordaroWagnerCode(5),
-        komm.ReedMullerCode(1, 4),
-    ]
+from komm._error_control_decoders.GaussianEliminationDecoder import (
+    _codeword_from_erased,
+    _codeword_from_known,
+    _message_from_erased,
+    _message_from_known,
 )
-def code(request: pytest.FixtureRequest) -> komm.abc.BlockCode:
-    return request.param
+
+small_codes = [
+    komm.HammingCode(3),
+    komm.BlockCode([[1, 0, 0, 1, 1], [0, 1, 1, 1, 0]]),
+    komm.BlockCode([[1, 1, 0, 1, 0], [0, 1, 1, 0, 1]]),
+    komm.BlockCode([[1, 0, 1, 1, 1], [1, 1, 0, 0, 1], [0, 1, 1, 0, 1]]),
+]
+
+all_codes = small_codes + [
+    komm.RepetitionCode(5),
+    komm.SingleParityCheckCode(5),
+    komm.CordaroWagnerCode(5),
+    komm.ReedMullerCode(1, 4),
+]
 
 
+@pytest.mark.parametrize("code", all_codes)
 def exhaustive_bitwise_map_decoder(code: komm.abc.BlockCode, b):
     codewords = code.codewords()
     compat_v = codewords[np.all((b == 2) | (b == codewords), axis=1)]
@@ -30,9 +37,7 @@ def exhaustive_bitwise_map_decoder(code: komm.abc.BlockCode, b):
     return v_hat, u_hat
 
 
-@pytest.mark.parametrize(
-    "code", [komm.HammingCode(3), komm.BlockCode([[1, 0, 0, 1, 1], [0, 1, 1, 1, 0]])]
-)
+@pytest.mark.parametrize("code", small_codes)
 def test_gaussian_elimination_exhaustive(code: komm.abc.BlockCode):
     decoder = komm.GaussianEliminationDecoder(code)
     for u in product([0, 1], repeat=code.dimension):
@@ -44,6 +49,24 @@ def test_gaussian_elimination_exhaustive(code: komm.abc.BlockCode):
             np.testing.assert_array_equal(decoder.decode_to_codeword(b), v_hat)
 
 
+@pytest.mark.parametrize("code", small_codes)
+def test_gaussian_elimination_both_paths_agree(code: komm.abc.BlockCode):
+    G, H = code.generator_matrix, code.check_matrix
+    G_r_inv = code.generator_matrix_right_inverse
+    for v in code.codewords():
+        for mask in product([False, True], repeat=code.length):
+            r = np.where(mask, 2, v)
+            np.testing.assert_array_equal(
+                _codeword_from_erased(H, r),
+                _codeword_from_known(G, r),
+            )
+            np.testing.assert_array_equal(
+                _message_from_erased(H, G_r_inv, r),
+                _message_from_known(G, r),
+            )
+
+
+@pytest.mark.parametrize("code", all_codes)
 def test_gaussian_elimination_correct_bits(code: komm.abc.BlockCode):
     # Every returned bit matches the true message.
     dms = komm.DiscreteMemorylessSource(2)
@@ -57,6 +80,7 @@ def test_gaussian_elimination_correct_bits(code: komm.abc.BlockCode):
         np.testing.assert_equal(u_hat[known], u[known])
 
 
+@pytest.mark.parametrize("code", all_codes)
 def test_gaussian_elimination_unique(code: komm.abc.BlockCode):
     # Fewer erasures than the minimum distance means unique decoding.
     dms = komm.DiscreteMemorylessSource(2)
@@ -69,6 +93,7 @@ def test_gaussian_elimination_unique(code: komm.abc.BlockCode):
             np.testing.assert_equal(decoder.decode(r), u)
 
 
+@pytest.mark.parametrize("code", all_codes)
 def test_gaussian_elimination_no_erasures(code: komm.abc.BlockCode):
     # Without erasures, every message is recovered.
     decoder = komm.GaussianEliminationDecoder(code)
