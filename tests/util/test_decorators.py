00c38ca7fea368d142daf1b96ead1f3143ba3420
Roberto Nobrega <rwnobrega@gmail.com>
2026-09-25 15:11:37 -0300

    test: add tests for `vectorize`, `blockwise`, and `with_pbar`

--- tests/util/test_decorators.py
+++ tests/util/test_decorators.py
@@ -1,9 +1,34 @@
 from io import StringIO
 
 import numpy as np
+import pytest
 from tqdm import tqdm
 
-from komm._util.decorators import chunkwise
+from komm._util.decorators import blockwise, chunkwise, vectorize, with_pbar
+
+
+def test_vectorize():
+    @vectorize
+    def func(row):
+        assert row.ndim == 1
+        return row[::2]
+
+    arr = np.arange(40).reshape(2, 5, 4)
+    np.testing.assert_equal(func(arr), arr[..., ::2])
+    np.testing.assert_equal(func(arr[0, 0]), arr[0, 0, ::2])
+
+
+def test_blockwise():
+    @blockwise(3)
+    def func(blocks):
+        assert blocks.shape[-1] == 3
+        return blocks.sum(axis=-1, keepdims=True)
+
+    np.testing.assert_equal(func([1, 2, 3, 4, 5, 6]), [6, 15])
+    arr = np.arange(24).reshape(2, 2, 6)
+    np.testing.assert_equal(func(arr), arr.reshape(2, 2, 2, 3).sum(axis=-1))
+    with pytest.raises(ValueError):
+        func([1, 2, 3, 4])
 
 
 def test_chunkwise():
@@ -19,3 +44,15 @@ def test_chunkwise():
     np.testing.assert_equal(func(arr), arr[..., ::2])
     assert sizes == [3, 3, 3, 1]
     assert pbar.n == 10
+
+
+def test_with_pbar():
+    pbar = tqdm(total=2, file=StringIO())
+
+    @with_pbar(pbar)
+    def func(arr):
+        return 2 * arr
+
+    np.testing.assert_equal(func(np.array([1, 2])), [2, 4])
+    np.testing.assert_equal(func(np.array([3])), [6])
+    assert pbar.n == 2
